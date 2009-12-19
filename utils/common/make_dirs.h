8fdf23c7ae8e3f37eff31e9c0c08a5cd25c7312d
gda <gda@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2009-12-19 10:32:21 +0000

    make_dirs was not found
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@253 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/common/make_dirs.h
+++ utils/common/make_dirs.h
@@ -1,6 +1,12 @@
 #ifndef __MAKE_DIRS_H__
 #define __MAKE_DIRS_H__
 
-int make_dirs(const char *FileName);
+#ifdef __cplusplus
+extern "C" {
+#endif 
+  int make_dirs(const char *FileName);
+#ifdef __cplusplus
+}
+#endif 
 
 #endif //__MAKE_DIRS_H__
