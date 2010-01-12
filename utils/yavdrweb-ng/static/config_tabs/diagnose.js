06cedb0e41b81188066e6f51798c41bffb17c5e8
hepi <hepi@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-01-12 18:36:13 +0000

    - web-frontend: re-arranged diagnose module, using tabPanels to get a better structure and better usability
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@331 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/yavdrweb-ng/static/config_tabs/diagnose.js
+++ utils/yavdrweb-ng/static/config_tabs/diagnose.js
@@ -1,77 +1,210 @@
 function yaVDRLogFilePanel(info, cmd, file)
 { 
-    return {
-        title: info,
-        iconCls: 'x-icon-templates',
-        tabTip: 'logfile tabtip' + file,
-        style: 'padding: 20px 30px 20px 30px;',
-        layout:'fit',
+    return new Ext.Panel({
+        id: 'diagnose_panel_wrapper_'+file,
+        title: info + ' (' + file + ')',
+        //layout: 'fit',
+        frame: true,
+        border: true,
+        //bodyBorder: true,
+        bodyStyle: 'border: 1px solid lightgray; background-color: white; padding: 2px;',
+        autoScroll: true,
+        tbar: [{
+            text: 'Aktualisieren',
+            icon: 'ext/resources/images/default/grid/refresh.gif',
+            tooltip: 'Klicken Sie diesen Button, um den Inhalt des Panels zu aktualisieren.',
+            handler: function(){
+                var thisObj = Ext.getCmp('diagnose_panel_'+file).getUpdater();
+                if(thisObj) thisObj.refresh();
+            }
+        },'-',{
+            text: 'Ans Ende springen',
+            icon: 'ext/resources/images/default/layout/ns-expand.gif',
+            tooltip: 'Klicken Sie diesen Button, um an das Endes des Inhalts dieses Panels zu springen.',
+            handler: function(){
+                var d = Ext.getCmp('diagnose_panel_wrapper_'+file).body.dom;
+                d.scrollTop = d.scrollHeight - d.offsetHeight;
+            }
+        }],
         items: [
             new Ext.Panel({
-                id: 'diagnose_panel_wrapper_'+file,
-                title: info + ' (' + file + ')',
-                frame: true,
-                border: true,
-                //bodyBorder: true,
-                bodyStyle: 'border: 1px solid lightgray; background-color: white; padding: 2px;',
-                autoScroll: true,
-                tbar: [{
-                    text: 'Aktualisieren',
-                    icon: 'ext/resources/images/default/grid/refresh.gif',
-                    tooltip: 'Klicken Sie diesen Button, um den Inhalt des Panels zu aktualisieren.',
-                    handler: function(){
-                        var thisObj = Ext.getCmp('diagnose_panel_'+file).getUpdater();
-                        if(thisObj) thisObj.refresh();
-                    }
-                },'-',{
-                    text: 'Ans Ende springen',
-                    icon: 'ext/resources/images/default/layout/ns-expand.gif',
-                    tooltip: 'Klicken Sie diesen Button, um an das Endes des Inhalts dieses Panels zu springen.',
-                    handler: function(){
-                        var d = Ext.getCmp('diagnose_panel_wrapper_'+file).body.dom;
-                        d.scrollTop = d.scrollHeight - d.offsetHeight;
-                    }
-                }],
-                items: [
-                    new Ext.Panel({
-                        id: 'diagnose_panel_'+file,
-                        layout: 'fit',
-                        frame: false,
-                        border: false,
-                        style: 'font-family: monospace; white-space: pre; font-size: 12px;',
-                        autoLoad: cmd + '?' + (cmd === 'get_file_content' ? 'file' : 'command') + '='+file
-                    })
-                ]
+                id: 'diagnose_panel_'+file,
+                layout: 'fit',
+                frame: false,
+                border: false,
+                style: 'font-family: monospace; white-space: pre; font-size: 12px;',
+                autoLoad: cmd + '?' + (cmd === 'get_file_content' ? 'file' : 'command') + '='+file
             })
         ]
-    };
+    });
 }
 
 function getDiagnoseItems(){
     return [
         {
             title: 'Diagnose',
+            layout: 'fit',
             iconCls: 'x-icon-configuration',
-            tabTip: 'Logfiles tabtip',
-            style: 'padding: 10px;',
+            tabTip: 'Diagnose tabtip',
+            style: 'padding: 20px 30px 20px 30px;',
             frame: false,
             border: false,
             html: '<p style="font-family: sans-serif;">Inhalte von wichtigen Logfiles und Konfigurationsdateien</p>' 
         },
-        yaVDRLogFilePanel('Netzwerkstatus', 'get_shell_response', 'ifconfig'),
-        yaVDRLogFilePanel('Systemstatus (top)', 'get_shell_response', 'top'),
-        yaVDRLogFilePanel('VDR: Setup', 'get_file_content', '/etc/vdr/setup.conf'),
-        yaVDRLogFilePanel('VDR: Fernbedienung','get_file_content', '/etc/vdr/remote.conf'),
-        yaVDRLogFilePanel('LIRC: Hardware-Konfiguration','get_file_content', '/etc/lirc/hardware.conf'),
-        yaVDRLogFilePanel('LIRC: lircd-Konfiguration', 'get_file_content', '/etc/lirc/lircd.conf'),
-        yaVDRLogFilePanel('X-Server Konfiguration','get_file_content', '/etc/X11/xorg.conf.yavdr'),
-        yaVDRLogFilePanel('XBMC-Logfile','get_file_content', '/home/'+ user + '/.xbmc/temp/xbmc.log'),
-        yaVDRLogFilePanel('XBMC-Logfile (old)','get_file_content', '/home/'+ user + '/.xbmc/temp/xbmc.old.log'),
-        yaVDRLogFilePanel('System-Logfile: messages','get_file_content', '/var/log/messages'),
-        yaVDRLogFilePanel('System-Logfile: user.log','get_file_content', '/var/log/user.log'),
-        yaVDRLogFilePanel('System-Logfile: syslog','get_file_content', '/var/log/syslog'),
-        yaVDRLogFilePanel('Webserver-Logfile (tntnet)','get_file_content', '/var/log/tntnet/tntnet.log'),
-        yaVDRLogFilePanel('yaVDR Datenbank', 'get_file_content', '/var/lib/yavdrdb.hdf')
+        {
+            title: 'System-Informationen',
+            layout: 'fit',
+            iconCls: 'x-icon-configuration',
+            tabTip: 'Netzwerk-Status, Auslastung und Prozesse',
+            style: 'padding: 20px 30px 20px 30px;',
+            frame: false,
+            border: false,
+            items: [
+                new Ext.TabPanel({
+                    frame: false,
+                    border: false,
+                    plain: true,
+                    activeTab: 0,
+                    defaults:{autoScroll: true},
+                    items: [
+                            yaVDRLogFilePanel('Netzwerkstatus', 'get_shell_response', 'ifconfig'),
+                            yaVDRLogFilePanel('Systemstatus (top)', 'get_shell_response', 'top')
+                    ]
+                })
+            ]
+        },
+        
+        {
+            title: 'System-Logfiles',
+            layout: 'fit',
+            iconCls: 'x-icon-configuration',
+            tabTip: 'Wichtige System-Logfiles',
+            style: 'padding: 20px 30px 20px 30px;',
+            frame: false,
+            border: false,
+            items: [
+                new Ext.TabPanel({
+                    frame: false,
+                    border: false,
+                    plain: true,
+                    activeTab: 0,
+                    defaults:{autoScroll: true},
+                    items: [
+                            yaVDRLogFilePanel('System-Logfile: messages','get_file_content', '/var/log/messages'),
+                            yaVDRLogFilePanel('System-Logfile: user.log','get_file_content', '/var/log/user.log'),
+                            yaVDRLogFilePanel('System-Logfile: syslog','get_file_content', '/var/log/syslog'),
+                    ]
+                })
+            ]
+        },
+        {
+            title: 'XBMC-Crashes',
+            layout: 'fit',
+            iconCls: 'x-icon-configuration',
+            tabTip: 'XBMC Logfiles',
+            style: 'padding: 20px 30px 20px 30px;',
+            frame: false,
+            border: false,
+            items: [
+                new Ext.TabPanel({
+                    frame: false,
+                    border: false,
+                    plain: true,
+                    activeTab: 0,
+                    defaults:{autoScroll: true},
+                    items: [
+                        yaVDRLogFilePanel('XBMC-Logfile','get_file_content', '/home/'+ user + '/.xbmc/temp/xbmc.log'),
+                        yaVDRLogFilePanel('XBMC-Logfile (old)','get_file_content', '/home/'+ user + '/.xbmc/temp/xbmc.old.log'),
+                    ]
+                })
+            ]
+        },
+        {
+            title: 'yaVDR-Utils',
+            layout: 'fit',
+            iconCls: 'x-icon-configuration',
+            tabTip: 'yaVDR-Utils Diagnose (Datenbank + Web-Server)',
+            style: 'padding: 20px 30px 20px 30px;',
+            frame: false,
+            border: false,
+            items: [
+                new Ext.TabPanel({
+                    frame: false,
+                    border: false,
+                    plain: true,
+                    activeTab: 0,
+                    defaults:{autoScroll: true},
+                    items: [
+                        yaVDRLogFilePanel('yaVDR Datenbank', 'get_file_content', '/var/lib/yavdrdb.hdf'),
+                        yaVDRLogFilePanel('Webserver-Logfile (tntnet)','get_file_content', '/var/log/tntnet/tntnet.log')
+                    ]
+                })
+            ]
+        },
+        {
+            title: 'LIRC-Konfiguration',
+            layout: 'fit',
+            iconCls: 'x-icon-configuration',
+            tabTip: 'LIRC-Problemdiagnose',
+            style: 'padding: 20px 30px 20px 30px;',
+            frame: false,
+            border: false,
+            items: [
+                new Ext.TabPanel({
+                    frame: false,
+                    border: false,
+                    plain: true,
+                    activeTab: 0,
+                    defaults:{autoScroll: true},
+                    items: [
+                        yaVDRLogFilePanel('LIRC: Hardware-Konfiguration','get_file_content', '/etc/lirc/hardware.conf'),
+                        yaVDRLogFilePanel('LIRC: lircd-Konfiguration', 'get_file_content', '/etc/lirc/lircd.conf')
+                    ]
+                })
+            ]
+        },
+        {
+            title: 'VDR-Konfiguration',
+            layout: 'fit',
+            iconCls: 'x-icon-configuration',
+            tabTip: 'VDR-Konfiguration',
+            style: 'padding: 20px 30px 20px 30px;',
+            frame: false,
+            border: false,
+            items: [
+                new Ext.TabPanel({
+                    frame: false,
+                    border: false,
+                    plain: true,
+                    activeTab: 0,
+                    defaults:{autoScroll: true},
+                    items: [
+                        yaVDRLogFilePanel('VDR: Setup', 'get_file_content', '/etc/vdr/setup.conf'),
+                        yaVDRLogFilePanel('VDR: Fernbedienung','get_file_content', '/etc/vdr/remote.conf')
+                    ]
+                })
+            ]
+        },
+        {
+            title: 'X-Server',
+            layout: 'fit',
+            iconCls: 'x-icon-configuration',
+            tabTip: 'LIRC-Problemdiagnose',
+            style: 'padding: 20px 30px 20px 30px;',
+            frame: false,
+            border: false,
+            items: [
+                new Ext.TabPanel({
+                    frame: false,
+                    border: false,
+                    plain: true,
+                    activeTab: 0,
+                    defaults:{autoScroll: true},
+                    items: [
+                        yaVDRLogFilePanel('X-Server Konfiguration','get_file_content', '/etc/X11/xorg.conf.yavdr')
+                    ]
+                })
+            ]
+        }
     ];
 }
-    
