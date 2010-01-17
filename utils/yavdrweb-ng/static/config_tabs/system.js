8949c222601daaabb6c1cebcb9b4319b4a686fb1
hepi <hepi@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-01-17 16:12:03 +0000

    web-frontend: added button "restart vdr-service" to page system (untestet)
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@369 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/yavdrweb-ng/static/config_tabs/system.js
+++ utils/yavdrweb-ng/static/config_tabs/system.js
@@ -9,12 +9,46 @@ function getSystemForm(){
         defaultType: 'textfield',
         buttonAlign: 'left'
     });
+/*
+    var submit_vdr_stop = myform.addButton({
+        text: 'VDR stoppen',
+        icon: 'ext/resources/images/default/grid/refresh.gif',
+        handler: function() {
+            myform.form.submit({
+                url: 'set_signal?signal=stop_vdr',
+                waitMsg:'Das Signal zum Stoppen des VDR wird abgesetzt.',
+                scope:this,
+                success: function (form, action) {
+                    Ext.MessageBox.alert('Message', 'Der VDR wird nun gestoppt.');
+                },
+                failure:function(form, action) {
+                    Ext.MessageBox.alert('Message', 'Fehler beim Absetzen des Signals. Bitte noch einmal versuchen.');
+                }
+            })
+        }
+    });
+    */
+    var submit_vdr_restart = myform.addButton({
+        text: 'VDR-Service neu starten',
+        icon: 'ext/resources/images/default/grid/refresh.gif',
+        handler: function() {
+            myform.form.submit({
+                url: 'set_signal?signal=restart-vdr',
+                waitMsg:'Das Signal zum Neustarten des VDR wird abgesetzt.',
+                scope:this,
+                success: function (form, action) {
+                    Ext.MessageBox.alert('Message', 'Der VDR wird nun neu gestartet.');
+                },
+                failure:function(form, action) {
+                    Ext.MessageBox.alert('Message', 'Fehler beim Absetzen des Signals. Bitte noch einmal versuchen.');
+                }
+            })
+        }
+    });
 
-    var submit = myform.addButton({
+    var submit_reboot = myform.addButton({
         text: 'Rechner neu starten (Vorsicht: Keine Sicherheitsabfrage!)',
         icon: 'ext/resources/images/default/grid/refresh.gif',
-        //formBind: true,
-        //scope: this,
         handler: function() {
             myform.form.submit({
                 url: 'set_signal?signal=reboot',
