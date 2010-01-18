3bdf76ebc9ec0a09b2c0f9fb49bc1fa3692457d0
gda <gda@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-01-18 12:05:46 +0000

    removed a possible race condition, code now more robust
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@371 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/yavdrweb-ng/auth.c
+++ utils/yavdrweb-ng/auth.c
@@ -9,13 +9,16 @@
 //#include "misc.h"
 #include "auth.h"
 
-char *pam_user;
-char *pam_pass;
+typedef struct {
+  const char *pam_user;
+  const char *pam_pass;
+} tAppdata;
 
 static int pam_exchange(int num_msg, const struct pam_message **msg, struct pam_response **resp, void *appdata_ptr)
 {
   int i;
   struct pam_response *response = NULL;
+  tAppdata *pAppdata = (tAppdata *)appdata_ptr;
   
   if(num_msg <= 0)
     return PAM_CONV_ERR;
@@ -33,14 +36,14 @@ static int pam_exchange(int num_msg, const struct pam_message **msg, struct pam_
       /* PAM frees response and resp.  If you don't believe this, please read
        * the actual PAM RFCs as well as have a good look at libpam.
        */
-      response[i].resp = pam_user ? (char *) strdup(pam_user) : NULL;
+      response[i].resp = pAppdata->pam_user ? (char *) strdup(pAppdata->pam_user) : NULL;
       break;
       
     case PAM_PROMPT_ECHO_OFF:
       /* PAM frees response and resp.  If you don't believe this, please read
        * the actual PAM RFCs as well as have a good look at libpam.
        */
-      response[i].resp = pam_pass ? (char *) strdup(pam_pass) : NULL;
+      response[i].resp = pAppdata->pam_pass ? (char *) strdup(pAppdata->pam_pass) : NULL;
       break;
       
     case PAM_TEXT_INFO:
@@ -62,32 +65,23 @@ static int pam_exchange(int num_msg, const struct pam_message **msg, struct pam_
   return PAM_SUCCESS;
 }
 
-static struct pam_conv pam_conv = {
-  &pam_exchange,
-  NULL
-};
 
+int authenticate_user(const char *user, const char *pass)
+{
+  pam_handle_t *pamh;
+  tAppdata appdata = {user, pass};
+  struct pam_conv pam_conv = {&pam_exchange, &appdata};
 
+  if ((pam_start (PAM_SERVICE_NAME, NULL, &pam_conv, &pamh) != PAM_SUCCESS) ||
+      (pam_authenticate (pamh,PAM_SILENT) != PAM_SUCCESS) ||
+      (pam_acct_mgmt (pamh,0) != PAM_SUCCESS) ||
+      (pam_setcred (pamh,PAM_ESTABLISH_CRED) != PAM_SUCCESS))
+  {
+    pam_end (pamh,PAM_AUTH_ERR); 
+    return 0;
+  }	
 
-int authenticate_user(const char *user, const char *pass)  {
-	pam_handle_t *pamh;
-
-	pam_user = (char *) strdup(user);
-	pam_pass = (char *) strdup(pass);
-
-	if   ((pam_start (PAM_SERVICE_NAME, NULL, &pam_conv, &pamh) != PAM_SUCCESS) ||
-	      (pam_authenticate (pamh,PAM_SILENT) != PAM_SUCCESS) ||
-	      (pam_acct_mgmt (pamh,0) != PAM_SUCCESS) ||
-	      (pam_setcred (pamh,PAM_ESTABLISH_CRED) != PAM_SUCCESS)) {
-	  		pam_end (pamh,PAM_AUTH_ERR); 
-			free(pam_user);
-			free(pam_pass);
-			return 0;
-	   		}	
-
-  	pam_end (pamh,PAM_SUCCESS);	
-	free(pam_user);
-	free(pam_pass);
-	return 1;	
+  pam_end (pamh,PAM_SUCCESS);	
+  return 1;	
 }
 
