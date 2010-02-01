84cdccddad82a6dded8a10c004ac4bebece0449e
ares <ares@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-02-01 18:42:20 +0000

    made nfs loader a bit more reliable
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@484 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

---
+++ startup/mount-loader.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function checkmount()
+{
+    retry=3
+    while [ $retry -gt 0 ]; do
+        logger "testing for $1"
+        if [ -d "$1" ]; then
+            logger "found $1"
+            MOUNTS="$MOUNTS -v $1"
+	    break
+        else
+            logger "sleeping"
+	    sleep 1
+            retry=$(expr $retry - 1)
+        fi
+    done
+}
+
+logger "executing $0"
+if [ -e /etc/auto.net.yavdr ]; then
+    logger "searching for video directories"
+    for d in `cat /etc/auto.net.yavdr | awk '!/^#/ && /video/ {print $1}'`; do
+        checkmount /net/$d
+    done
+    logger "adding \$MOUNTS to vdr: \"$MOUNTS\""
+    OPTIONS="$OPTIONS $MOUNTS"
+fi
+
