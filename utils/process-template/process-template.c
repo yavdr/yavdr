a71a103e8143d794e4cfc78aba0bd8d7cde10db3
gda <gda@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-02-02 10:03:43 +0000

    support for custom templates
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@487 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/process-template/process-template.c
+++ utils/process-template/process-template.c
@@ -87,21 +87,44 @@ int scandirfilter(const struct dirent *entry)
   return entry->d_name[0] - '.';
 }
 
-int main(int argc, char *argv[])
+int write_segment(int templatefd, char *segmentname) 
+{
+  int readlen;
+  char buffer[1024];
+  int ret = 0;
+  int segmentfd = 0;
+
+  if ((segmentfd = open(segmentname, O_RDONLY)) < 0)
+  {
+    perror("open");
+    ret = -1;
+  }
+  else
+  {
+    while ((readlen = read(segmentfd, buffer, sizeof(buffer))) > 0)
+    {
+      write(templatefd, buffer, readlen);
+    }
+    close(segmentfd);
+  }
+  return ret;
+}
+
+int merge_template(char *template)
 {
   struct dirent **namelist;
+  struct dirent **namelistcustom;
   int numtemps;
-  int n;
+  int numcustomtemps;
+  int n, m, comp;
   char *templatedir;
+  char *templatecustomdir;
   char templatename[] = "/tmp/template.XXXXXX";
   int templatefd = 0;
   char *segmentname;
-  int segmentfd = 0;
   int ret = 0;
-  int readlen;
-  char buffer[1024];
  
-  if (asprintf(&templatedir, "%s/%s", TEMPLATEPATH, argv[1]) < 0)
+  if (asprintf(&templatedir, "%s/%s", TEMPLATEPATH, template) < 0)
   {
     perror("asprintf");
     ret = -1;
@@ -117,54 +140,122 @@ int main(int argc, char *argv[])
     }
     else
     {
-      if ((templatefd = mkstemp(templatename)) < 0)
+      if (asprintf(&templatecustomdir, "%s/%s", TEMPLATECUSTOMPATH, template) < 0)
       {
-        perror("mkstemp");
+        perror("asprintf");
         ret = -3;
       }
-   
-      for (n = 0; n < numtemps; n++)
-      {
-        if (asprintf(&segmentname, "%s/%s", templatedir, namelist[n]->d_name) < 0)
+      else
+      {  
+        numcustomtemps = scandir(templatecustomdir, &namelistcustom, scandirfilter, alphasort);
+
+        if (numcustomtemps < 0)
         {
-          perror("asprintf");
+          perror("scandir");
           ret = -4;
         }
         else
         {
-          if ((segmentfd = open(segmentname, O_RDONLY)) < 0)
+          if ((templatefd = mkstemp(templatename)) < 0)
           {
-            perror("open");
+            perror("mkstemp");
             ret = -5;
           }
-          else
+   
+          for (n = 0, m = 0; n < numtemps; n++)
           {
-            while ((readlen = read(segmentfd, buffer, sizeof(buffer))) > 0)
+            while ((ret == 0) && (m < numcustomtemps) && 
+                   ((comp = strcmp(namelist[n]->d_name, namelistcustom[m]->d_name)) >= 0))
             {
-              write(templatefd, buffer, readlen);
-            }
-            close(segmentfd);
-          }
-          free(segmentname);
-        }
-        free(namelist[n]);
-      }
-      free(namelist);
-      
-      if (templatefd > 0)
-      {
-        close(templatefd);
-      }
-    }
-    free(templatedir);
+              if (asprintf(&segmentname, "%s/%s", templatecustomdir, namelistcustom[m]->d_name) < 0)
+              {
+                perror("asprintf");
+                ret = -6;
+              }
+              else
+              {
+                if (write_segment(templatefd, segmentname))
+                {
+                  ret = -7;
+                }
+                free(segmentname);
+              }
+              free(namelistcustom[m++]);
 
-    if (ret ==0)
-    {
-      if (process_template(templatename, argv[1]) < 0)
-      {
-        ret = -6;
+              if (comp == 0)
+              {
+                break;
+              }
+            }
+            if (comp != 0)
+            {
+              if (asprintf(&segmentname, "%s/%s", templatedir, namelist[n]->d_name) < 0)
+              {
+                perror("asprintf");
+                ret = -8;
+              }
+              else
+              {
+                if (write_segment(templatefd, segmentname))
+                {
+                  ret = -9;
+                }
+                free(segmentname);
+              }
+            }
+            free(namelist[n]);
+          }
+          free(namelist);
+      
+          while ((ret == 0) && (m < numcustomtemps))
+          {
+            if (asprintf(&segmentname, "%s/%s", templatecustomdir, namelistcustom[m]->d_name) < 0)
+            {
+              perror("asprintf");
+              ret = -10;
+            }
+            else
+            {
+              if (write_segment(templatefd, segmentname))
+              {
+                ret = -11;
+              }
+              free(segmentname);
+            }
+            free(namelistcustom[m++]);
+          }
+
+          if (numcustomtemps > 0)
+          {
+            free(namelistcustom);
+          }
+
+          if (templatefd > 0)
+          {
+            close(templatefd);
+          }
+        }
+        free(templatedir);
+
+        if (ret == 0)
+        {
+          if (process_template(templatename, template) < 0)
+          {
+            ret = -12;
+          }
+        }
       }
     }  
   }  
+  return ret;
+}
+
+
+int main(int argc, char *argv[])
+{
+  int ret = 0;
+
+  ret = merge_template(argv[1]);
+  
   return ret;
 }
