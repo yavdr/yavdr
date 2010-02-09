460323450c7d5aaf3b836d04010a60646d3f4e25
ares <ares@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-02-09 21:04:59 +0000

    some reorg of nfs panel ability to add new dirs
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@583 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/yavdrweb-ng/static/config_tabs/nfs.js
+++ utils/yavdrweb-ng/static/config_tabs/nfs.js
@@ -1,64 +1,111 @@
 function getNFSForm(){
-    var store = new Ext.data.ArrayStore({
-	    fields: [ { name:'netspec', type: 'string'} ]
+    var rt = Ext.data.Record.create([{name: 'netspec'}]);
+    
+    var store = new Ext.data.Store({
+	    url: 'get_autofs_config?cmd=mounts',
+	    reader: new Ext.data.ArrayReader( { idIndex: 0 },
+					      rt )
 	});
 
-    var mounts = new Ext.grid.GridPanel({
-	    store: store,
-	    multiSelect: false,
-	    stripeRows: true,
-	    autoExpandColumn: 'netspec',
-	    height: 200,
-	    width: 400,
-	    title: 'NFS mounts',
-	    // config options for stateful behavior
-	    stateful: true,
-	    stateId: 'grid',
-	    columns: [{
-		    id: 'netspec',
-		    header: 'Remote Spec',
-		    width: 400,
-		    dataIndex: 'netspec'
-		}]
-	});
-	    
-    var myform = new Ext.FormPanel({
-        frame: false,
-        plain: false,
-        border: true,
-        bodyStyle:'padding:5px 5px 0;',
-        labelWidth: 150,
-        buttonAlign: 'left',
-        items: mounts
-	});
-
-    var submit = myform.addButton({
-        text: 'Übernehmen',
-        handler: function() {
-            myform.form.submit({
-                url: 'set_signal?signal=change-auto-net-yavdr',
-                waitMsg: 'wait',
-                waitTitle: locale.standardform.messagebox_caption.wait,
-                scope:this,
-                success: function (form, action) {
-                    Ext.MessageBox.alert( locale.standardform.messagebox_caption.message, locale.upload.submit.success );
-                },
-                failure:function(form, action) {
-                    Ext.MessageBox.alert( locale.standardform.messagebox_caption.error, locale.upload.submit.failure );
-                }
-            })
-        }
-    });
     Ext.Ajax.request({
 	    url: 'get_autofs_config?cmd=mounts',
 		timeout: 3000,
-		method: 'GET',
+		method:  'GET',
 		success: function(xhr) {
-		// alert('Response is "' + xhr.responseText + '"');
-		var data = Ext.util.JSON.decode( xhr.responseText );
-		store.loadData( data );
+		myData = Ext.util.JSON.decode( xhr.responseText );
+		store.loadData( myData );
             }
 	});
 
-    return myform;
+    var mounts = new Ext.grid.GridPanel({
+	    store:       store,
+	    multiSelect: false,
+	    height:      100,
+	    // config options for stateful behavior
+	    columns: [{dataIndex: 'netspec'}]
+	});
+	    
+    var newDir = new Ext.form.TextField({
+	    fieldLabel: 'Remote directory',
+	    name: 'remote',
+	    anchor: '100%'
+	});
+
+    var mounttable = new Ext.Panel({
+	    layout:      'form',
+	    frame:       false,
+	    plain:       false,
+	    border:      false,
+	    labelWidth:  100,
+	    width:       400,
+
+ 	    items: [ newDir, mounts ]
+	});
+
+    var recId = 100;
+    var mountactions = new Ext.Panel({
+	    layout:       'anchor',
+	    frame:        false,
+	    border:       false,
+	    width:        100,
+	    layoutConfig: {columns:1},
+	    defaultType:  'button',
+	    items: [{
+		    text: 'Hinzufügen',
+		    anchor: '100%',
+		    listeners: {
+			click: function() {
+			    var r = new store.recordType({
+				    netspec: newDir.getValue()},
+				recId++);
+			    store.add( r );
+			}
+		    }
+		},{
+		    text: 'Editieren',
+		    anchor: '100%'
+		},{
+		    text: 'Löschen',
+		    anchor: '100%'
+		},{
+		    text: 'Übernehmen',
+		    anchor: '100%',
+		    listeners: {
+			click: function() {
+			    var mounts = [];
+			    Ext.each(store.getRange(), // complete
+				     function(k, v){
+					 mounts.push( k.data.netspec );},
+				     this);
+			    panel.form.submit({
+				    url: 'set_autofs_config',
+				    params: { 'cmd' : 'mounts',
+					      'mounts' : mounts },
+				    waitMsg: 'wait',
+				    waitTitle: locale.standardform.messagebox_caption.wait,
+				    scope:this,
+				    success: function (form, action) {
+					Ext.MessageBox.alert( locale.standardform.messagebox_caption.message, locale.upload.submit.success );
+				    },
+				    failure:function(form, action) {
+					Ext.MessageBox.alert( locale.standardform.messagebox_caption.error, locale.upload.submit.failure );
+				    }
+				})
+			}
+		    }
+		}]
+	});
+    
+    var panel = new Ext.FormPanel({
+	    layout:'column',
+	    frame: false,
+	    plain: false,
+	    border: true,
+	    bodyStyle:'padding:5px 5px 0;',
+	    labelWidth: 150,
+	    items: [mounttable,
+		    mountactions]
+	});
+
+    return panel;
 }
