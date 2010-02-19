ffd7fcae7516875b120c6d72f454fcf4e3588430
gda <gda@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-02-19 12:59:15 +0000

    signal-event is now more verbose and writes to syslog
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@641 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/signal-event/signal-event.c
+++ utils/signal-event/signal-event.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <dirent.h>
+#include <syslog.h>
+#include <sys/stat.h> 
 
 #include "common.h"
 
@@ -17,19 +20,25 @@ int main(int argc, char *argv[])
   int numcmds;
   int n;
   char *eventdir;
-  char *eventcmd;
+  char *action = NULL;
+  char *actioncmd = NULL;
   int i;
+
+  openlog(argv[0], LOG_PID | LOG_CONS, LOG_USER);
 
   if (argc < 2)
   {
+    syslog(LOG_ERR, "usage: %s <event> [argument] ...\n", argv[0]);
     fprintf(stderr, "usage: %s <event> [argument] ...\n", argv[0]);
     ret = -1;
   }
   else
   {
+    syslog(LOG_INFO, "processing signal %s", argv[1]);
+
     if (asprintf(&eventdir, "%s/%s", YAVDREVENTS, argv[1]) < 0)
     {
-      perror("asprintf");
+      syslog(LOG_ERR, "ERROR: asprintf %m");
       ret = -2;
     }
     else
@@ -37,47 +46,82 @@ int main(int argc, char *argv[])
       numcmds = scandir(eventdir, &namelist, scandirfilter, alphasort);
       if (numcmds < 0)
       {
-        perror("scandir");
+        syslog(LOG_ERR, "ERROR: scandir %m: %s", eventdir);
         ret = -3;
       }
       else
       {
         for (n = 0; n < numcmds; n++)
         {
-          if (asprintf(&eventcmd, "%s/%s %s", eventdir, namelist[n]->d_name, argv[1]) < 0)
+          if (action)
           {
-            perror("asprintf");
-            ret = -4;
+            free(action);
+            action = NULL;
+          }
+
+          if (asprintf(&action, "%s/%s", eventdir, namelist[n]->d_name) > 0)
+          {
+            if (access(action, F_OK | X_OK))
+            {
+              syslog(LOG_ERR, "ERROR: access %m: %s", action);
+              ret = -4;
+              continue;
+            }
+          }
+          else
+          {
+            syslog(LOG_ERR, "ERROR: asprintf %m");
+            ret = -5;
+          }
+
+          if (asprintf(&actioncmd, "%s %s", action, argv[1]) < 0)
+          {
+            syslog(LOG_ERR, "ERROR: asprintf %m");
+            ret = -6;
           }
           else
           {
             for (i = 2; i < argc; i++)
             {
-              if ((eventcmd = realloc(eventcmd, strlen(eventcmd) + strlen(argv[2]) + 2)) == NULL)
+              if ((actioncmd = realloc(actioncmd, strlen(actioncmd) + strlen(argv[2]) + 2)) == NULL)
               {
-                perror("realloc");
-                ret = -5;
+                syslog(LOG_ERR, "ERROR: realloc %m");
+                ret = -7;
                 break;
               }
               else
               {
-                 strcat(eventcmd, " ");
-                 strcat(eventcmd, argv[i]);
+                 strcat(actioncmd, " ");
+                 strcat(actioncmd, argv[i]);
               }
             }
-            if (eventcmd != NULL)
+            if (actioncmd != NULL)
             {
-              system(eventcmd);
-              free(eventcmd);
+              syslog(LOG_INFO, "processing action %s", actioncmd);
+              if (system(actioncmd) == -1)
+              {
+                syslog(LOG_ERR, "ERROR: error processing signal %s", actioncmd);
+              }
+              free(actioncmd);
             }
           }
           free(namelist[n]);
         }
+        if (action)
+        {
+          free(action);
+          action = NULL;
+        }
+
         free(namelist);
       }
       free(eventdir);
     }
   }
+
+  syslog(LOG_INFO, "finished processing signal %s", argc >= 2 ? argv[1] : "");
+
+  closelog();
 
   return ret;
 }
