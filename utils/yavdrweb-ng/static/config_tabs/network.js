516d360663b39a96f378d736a1db25737a9242af
ares <ares@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-03-15 21:22:47 +0000

    improve layout of nfs configuration
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@727 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/yavdrweb-ng/static/config_tabs/network.js
+++ utils/yavdrweb-ng/static/config_tabs/network.js
@@ -19,11 +19,78 @@ function getNFSForm(){
 	  }
       });
 
-  var mounts = new Ext.grid.GridPanel({
+  var newDir = new Ext.form.TextField({
+	  fieldLabel: getLL("network.nfs.labels.newDir"),
+	  name: 'remote',
+	  width: 200
+      });
+
+  var mounttable = new Ext.grid.GridPanel({
 	  store:       store,
 	  multiSelect: false,
-	  height:      100,
+	  height:      300,
 	  plain: false,
+	  frame: true,
+	  tbar: [ newDir
+		  ,{
+		  text: getLL("network.nfs.labels.add"),
+		  handler: function() {
+		      if( newDir.getValue().length ) {
+			  var r = new store.recordType({
+				  netspec: newDir.getValue()},
+			      recId++);
+			  store.add( r );
+			  selectedRow = -1;
+		      }
+		  }
+	      },{
+		  text: getLL("network.nfs.labels.edit"),
+		  handler: function() {
+		      if( 0 <= selectedRow ){
+			  var record = store.getAt( selectedRow );
+			  if( newDir.getValue().length ) {
+			      record.set( 'netspec', newDir.getValue() );
+			      newDir.setValue('');
+			      selectedRow = -1;
+			  }
+		      }
+		  }
+	      },{
+		  text: getLL("network.nfs.labels._delete"),
+		  handler: function() {
+		      if( 0 <= selectedRow ){
+			  var record = store.getAt( selectedRow );
+			  store.remove( record );
+			  newDir.setValue('');
+			  selectedRow = -1;
+		      }
+		  }
+	      },{
+		  text: getLL("network.nfs.labels.apply"),
+		  handler: function() {
+		      var mounts = [];
+		      selectedRow = -1;
+		      Ext.each(store.getRange(), // complete
+			       function(k, v){
+				   mounts.push( k.data.netspec );},
+			       this);
+		      Ext.Ajax.request({
+			      url: 'set_autofs_config',
+				  timeout: 3000,
+				  method:  'GET',
+				  params: { 'cmd' : 'mounts',
+				      'mounts' : mounts },
+				  waitMsg: 'wait',
+				  waitTitle: getLL("standardform.messagebox_caption.wait"),
+				  success: function(xhr) {
+				  Ext.MessageBox.alert( getLL("standardform.messagebox_caption.message"), getLL("upload.submit.success") );
+			      },
+				  failure:function(form, action) {
+				  Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), getLL("upload.submit.failure") );
+			      }
+			  })
+		  }
+	      }],
 	  // config options for stateful behavior
 	  columns: [{dataIndex: 'netspec'}],
 	  listeners: {
@@ -35,121 +102,13 @@ function getNFSForm(){
 	  }
       });
 	    
-  var newDir = new Ext.form.TextField({
-	  fieldLabel: getLL("network.nfs.labels.newDir"),
-	  name: 'remote',
-	  anchor: '100%'
-      });
-
-  var mounttable = new Ext.Panel({
-	  layout:      'form',
-	  frame:       false,
-	  plain:       false,
-	  border:      false,
-	  labelWidth:  150,
-	  width:       400,
-
-	  items: [ newDir, mounts ]
-      });
-
-  var mountactions = new Ext.Panel({
-	  layout:       'anchor',
-	  frame:        false,
-	  border:       false,
-	  width:        100,
-	  layoutConfig: {columns:1},
-	  defaultType:  'button',
-	  items: [{
-		  text: getLL("network.nfs.labels.add"),
-		  anchor: '100%',
-		  listeners: {
-		      click: function() {
-			  if( newDir.getValue().length ) {
-			      var r = new store.recordType({
-				      netspec: newDir.getValue()},
-				  recId++);
-			      store.add( r );
-			      selectedRow = -1;
-			  }
-		      }
-		  }
-	      },{
-		  text: getLL("network.nfs.labels.edit"),
-		  anchor: '100%',
-		  listeners: {
-		      click: function() {
-			  if( 0 <= selectedRow ){
-			      var record = store.getAt( selectedRow );
-			      if( newDir.getValue().length ) {
-				  record.set( 'netspec', newDir.getValue() );
-				  newDir.setValue('');
-				  selectedRow = -1;
-			      }
-			  }
-		      }
-		  }
-	      },{
-		  text: getLL("network.nfs.labels._delete"),
-		  anchor: '100%',
-		  listeners: {
-		      click: function() {
-			  if( 0 <= selectedRow ){
-			      var record = store.getAt( selectedRow );
-			      store.remove( record );
-			      newDir.setValue('');
-			      selectedRow = -1;
-			  }
-		      }
-		  }
-	      },{
-		  text: getLL("network.nfs.labels.apply"),
-		  anchor: '100%',
-		  listeners: {
-		      click: function() {
-			  var mounts = [];
-			  selectedRow = -1;
-			  Ext.each(store.getRange(), // complete
-				   function(k, v){
-				       mounts.push( k.data.netspec );},
-				   this);
-			  Ext.Ajax.request({
-				  url: 'set_autofs_config',
-				      timeout: 3000,
-				      method:  'GET',
-				      params: { 'cmd' : 'mounts',
-					  'mounts' : mounts },
-				      waitMsg: 'wait',
-				      waitTitle: getLL("standardform.messagebox_caption.wait"),
-				      success: function(xhr) {
-				      Ext.MessageBox.alert( getLL("standardform.messagebox_caption.message"), getLL("upload.submit.success") );
-				  },
-				      failure:function(form, action) {
-				      Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), getLL("upload.submit.failure") );
-				  }
-			      })
-		      }
-		  }
-	      }]
-      });
-    
   var description = new Ext.form.TextField({
 	  disabled: true,
 	  originalValue: getLL("network.nfs.help")
       });
 
-  var panel = new Ext.FormPanel({
-	  layout:'column',
-	  frame: false,
-	  plain: false,
-	  border: false,
-	  bodyStyle:'padding:5px 5px 0;',
-	  labelWidth: 150,
-	  items: [mounttable,
-		  mountactions]
-      });
-
   return [ 
-       panel,
+       mounttable,
        new Ext.Panel({
            width: "100%",
            frame: false,
