fc5488dd5c97fece3a4ff88ddf766ffcde2468f4
hepi <hepi@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-04-20 20:56:33 +0000

    web-frontend: * "ext/ext-all-debug.js" is loaded instead of "ext/ext-all.js" when devmode == 1 * refactored lirc.js in trunk (making it similar to current version in branch-0.1), hopefully fixing small js bugs (v_r, please test)
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@887 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/yavdrweb-ng/static/config_tabs/lirc.js
+++ utils/yavdrweb-ng/static/config_tabs/lirc.js
@@ -1,54 +1,57 @@
-function populateLircForm( form, lircData ){
-
-    var receiverStore = new Ext.data.ArrayStore({
-        storeId : 'lirc_receiver_store',
-        autoDestroy: true,
-        idIndex: 0,
-        fields: [
-                 "id",
-                 "description",
-                 "driver",
-                 "lirc_driver",
-                 "hw_default",
-                 "lircd_conf"
-                 ],
-        data : lircData.receiverlist,
-        sortInfo: {
-            field: 'description',
-            direction: 'ASC'
-        }
-    });
-    
-    var cbox = form.getComponent('lirc_receiver_combobox');
-    cbox.store = receiverStore;
-    cbox.hiddenValue = lircData.current_receiver;  //initial value, used in POST request
-    cbox.on({
-        'select' : {
-            fn: onLircComboBoxChange,
-            scope: this,
-            delay: 100
-        }
-    });
-    
-//    //if no serial port was chosen before, don't preselect
-//    if (lircData.current_serial_port != "")
-    form.getComponent('serial_port_radio_group').setValue(lircData.current_serial_port);
-
-    //if no receiver was chosen before, don't preselect
-    if (lircData.current_receiver != -1){
-        cbox.value = lircData.current_receiver;
-        var currentRecord = receiverStore.getById( lircData.current_receiver );
-        adjustSerialSettings(currentRecord.get('lirc_driver'), currentRecord.get('driver'));
-    }
-    
-    if (lircData.current_remoted == 'lircd') {
-        form.enable();
-    }
+/*
+ * getLircForm
+ */
+function getLircForm(){
+    var lircFormInstance = new lircForm();
+    lircFormInstance.loadJSONData();
+    return lircFormInstance.getExtjsForm();
 }
 
-function getLircForm(){
-    var myform = new Ext.form.RemoteFormPanel({
-        items:[{
+/*
+ * lircForm (constructor)
+ */
+function lircForm(){
+    
+    this.combobox = new Ext.form.ComboBox({ 
+        //id : 'lirc_receiver_combobox',
+        tpl: '<tpl for="."><div ext:qtip="' + 
+                getLL("lirc.combobox.tooltip.driver") + 
+                ': {driver}<br/'+'>' + 
+                getLL("lirc.combobox.tooltip.lirc_driver") + 
+                ': {lirc_driver}<br/'+
+                '>HW-Default: {hw_default}<'+'br/'+
+                '>Lircd-Conf: {lircd_conf}" class="x-combo-list-item">{description}</div></tpl>',
+        //name: ... used in POST request
+        hiddenName: 'receiver_id', //key, defined in set_lirchw.ecpp, used in POST request
+        //set per method hiddenValue: lircData.current_receiver,  //initial value, used in POST request
+        valueField: 'id', //value column, used in POST request
+        displayField:'description',
+        typeAhead: true,
+        forceSelection: true,
+        mode: "local",
+        triggerAction: 'all',
+        emptyText: getLL("lirc.combobox.emptytext"),
+        fieldLabel: getLL("lirc.combobox.label"),
+        selectOnFocus: true,
+        disabled: true
+    });
+    
+    this.serialPortGroup = new Ext.form.RadioGroup({
+        //id: 'serial_port_radio_group',
+        name: 'serial_port',
+        columns: 1,
+        fieldLabel: getLL("lirc.serial_radiogroup.label"),
+        items: [
+            {id: 'rb1', boxLabel: getLL("lirc.serial_radiogroup.boxlabel_none"), name: 'serial_port', inputValue: ''},
+            {id: 'rb2', boxLabel: '/dev/ttyS0', name: 'serial_port', inputValue: '/dev/ttyS0'},
+            {id: 'rb3', boxLabel: '/dev/ttyS1', name: 'serial_port', inputValue: '/dev/ttyS1'}
+        ],
+        disabled: true
+    });
+    
+    this.myform = new Ext.form.RemoteFormPanel({
+        items:[
+            {
                 xtype: 'radio',
                 name: 'remotetype',
                 inputValue: 'lircd',
@@ -60,51 +63,17 @@ function getLircForm(){
                     }
                 }
             },
-            new Ext.form.ComboBox({ 
-               id : 'lirc_receiver_combobox',
-               tpl: '<tpl for="."><div ext:qtip="' + 
-                       getLL("lirc.combobox.tooltip.driver") + 
-                       ': {driver}<br/'+'>' + 
-                       getLL("lirc.combobox.tooltip.lirc_driver") + 
-                       ': {lirc_driver}<br/'+
-                       '>HW-Default: {hw_default}<'+'br/'+
-                       '>Lircd-Conf: {lircd_conf}" class="x-combo-list-item">{description}</div></tpl>',
-               //name: ... used in POST request
-               hiddenName: 'receiver_id', //key, defined in set_lirchw.ecpp, used in POST request
-               //set per method hiddenValue: lircData.current_receiver,  //initial value, used in POST request
-               valueField: 'id', //value column, used in POST request
-               displayField:'description',
-               typeAhead: true,
-               forceSelection: true,
-               mode: "local",
-               triggerAction: 'all',
-               emptyText: getLL("lirc.combobox.emptytext"),
-               fieldLabel: getLL("lirc.combobox.label"),
-               selectOnFocus: true,
-               disabled: true
-           }),
-           {
-                id: 'serial_port_radio_group',
-                name: 'serial_port',
-                xtype: 'radiogroup',
-                columns: 1,
-                fieldLabel: getLL("lirc.serial_radiogroup.label"),
-                items: [
-                    {id: 'rb1', boxLabel: getLL("lirc.serial_radiogroup.boxlabel_none"), name: 'serial_port', inputValue: ''},
-                    {id: 'rb2', boxLabel: '/dev/ttyS0', name: 'serial_port', inputValue: '/dev/ttyS0'},
-                    {id: 'rb3', boxLabel: '/dev/ttyS1', name: 'serial_port', inputValue: '/dev/ttyS1'}
-                ],
-                disabled: true
-            }
+            this.combobox,
+            this.serialPortGroup
         ]
     });
-    
-    var submit = myform.addButton({
+
+    var submit = this.myform.addButton({
         text: getLL("standardform.button.save"),
         //formBind: true,
-        //scope: this,
+        scope: this,
         handler: function() {
-            myform.form.submit({
+            this.myform.form.submit({
                 url: 'set_lirchw',
                 tabTip: 'Test',
                 waitMsg: getLL("lirc.submit.waitmsg"),
@@ -120,12 +89,17 @@ function getLircForm(){
         },
         disabled: true
     });
-  
+}
+
+/*
+ * loadJSONData
+ */
+lircForm.prototype.loadJSONData = function (){
     Ext.Ajax.request({
         url: 'get_lirchwdb',
         timeout: 3000,
         method: 'GET',
-        scope: myform,
+        scope: this,
         success: function(xhr) {
             //alert('Response is "' + xhr.responseText + '"');
             var lircData = 0;
@@ -136,12 +110,66 @@ function getLircForm(){
             catch (err) {
                 Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), getLL("lirc.error.json_decode"));
             }            
-            populateLircForm( this, lircData );
+            this.lircData = lircData;
+            this.populateFields();
+        }
+    });
+}
+
+/*
+ * populateFields
+ */
+lircForm.prototype.populateFields = function (){
+
+    this.combobox.store = new Ext.data.ArrayStore({
+        storeId : 'lirc_receiver_store',
+        autoDestroy: true,
+        idIndex: 0,
+        fields: [
+                 "id",
+                 "description",
+                 "driver",
+                 "lirc_driver",
+                 "hw_default",
+                 "lircd_conf"
+                 ],
+        data : this.lircData.receiverlist,
+        sortInfo: {
+            field: 'description',
+            direction: 'ASC'
+        }
+    });
+    this.combobox.hiddenValue = this.lircData.current_receiver;  //initial value, used in POST request
+    this.combobox.on({
+        'select' : {
+            fn: this.onComboBoxChange,
+            scope: this,
+            delay: 100
         }
     });
 
+    //if no serial port was chosen before, don't preselect
+    if (this.lircData.current_serial_port != "")
+        this.serialPortGroup.setValue(this.lircData.current_serial_port);
+
+    //if no receiver was chosen before, don't preselect
+    if (this.lircData.current_receiver != -1){
+        this.combobox.setValue(this.lircData.current_receiver );
+        var currentRecord = this.combobox.store.getById( this.lircData.current_receiver );
+        this.adjustSerialSettings( currentRecord.get('lirc_driver'), currentRecord.get('driver'));
+    }
+    
+    if (this.lircData.current_remoted == 'lircd') {
+        this.myform.enable();
+    }
+}
+
+/*
+ * getExtjsForm
+ */
+lircForm.prototype.getExtjsForm = function(){
     return [ 
-        myform,
+        this.myform,
         new Ext.Panel({
             width: "100%",
             layout: 'fit',
@@ -152,23 +180,22 @@ function getLircForm(){
     ];
 }
 
-function onLircComboBoxChange( combo, record, index){
-    adjustSerialSettings(record.data.lirc_driver, record.data.driver );
+/*
+ * onComboBoxChange
+ */
+lircForm.prototype.onComboBoxChange = function( combo, record, index){
+    this.adjustSerialSettings( record.data.lirc_driver, record.data.driver );
 }
-    
-function adjustSerialSettings(lirc_driver, driver){
-    /*buggy
-    var radioGroup = Ext.getCmp('serial_port_radio_group');
-    if (!radioGroup) 
-        Ext.MessageBox.alert('ERROR', 'Could not find radiogroup');
+
+/*
+ * adjustSerialSettings
+ */
+lircForm.prototype.adjustSerialSettings = function(lirc_driver, driver){
     if (lirc_driver === "lirc_dev lirc_serial" || driver ===  "serial"){
-        //radioGroup.setVisible(true); radioGroup.show();
-        Ext.getCmp('serial_port_radio_group').setDisabled(false);
+        this.serialPortGroup.setDisabled(false);
     }
     else{
-        //radioGroup.setVisible(false); radioGroup.hide();
-        //Ext.getCmp('serial_port_radio_group').value = 'keine';
-        Ext.getCmp('serial_port_radio_group').setValue( '' );
-        Ext.getCmp('serial_port_radio_group').setDisabled(true);
-    }*/
+        this.serialPortGroup.setValue( '' );
+        this.serialPortGroup.setDisabled(true);
+    }
 }
