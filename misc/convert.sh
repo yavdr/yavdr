79eacf49eb13639134acd9b44caf13c348799fc2
holgerr <holgerr@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-08-11 19:20:10 +0000

    make 'no signal' smaller
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@1642 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- misc/convert.sh
+++ misc/convert.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-jpegtopnm cd.jpg | pnmscale -xsize=720 -ysize=576 | \
+jpegtopnm noSignal4x3.jpg  | pnmscale -xsize=720 -ysize=576 | \
 	ppmtoy4m -F 25:1 -A 4:3 -I p -r -S 420mpeg2 -v 2 -n 1 | \
-	mpeg2enc -f 8 -a 2 -q 1 -n p -T 120 -R 2 -g 12 -G 12 -o cd.mpg
+	mpeg2enc -f 8 -a 2 -q 1 -n p -T 120 -R 2 -g 12 -G 12 -o noSignal4x3.mpg
