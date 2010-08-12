511df39f475a74a4200428efd4284cbd28ef50ff
hotzenplotz5 <hotzenplotz5@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-08-12 18:58:10 +0000

    wrong link
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@1657 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- startup/yavdr-startup/events/actions/create-etc-init.d
+++ startup/yavdr-startup/events/actions/create-etc-init.d
@@ -3,5 +3,5 @@
 process-template --mode 775 /etc/init.d/force-fsck
 
 # Symlinks erstellen, die beim Shutdown ausgeführt werden:
-ln -sf /etc/init.d/force-fsck /etc/rc0.d/S61force-fchk
-ln -sf /etc/init.d/force-fsck /etc/rc6.d/S61force-fchk
+ln -sf /etc/init.d/force-fsck /etc/rc0.d/S61force-fschk
+ln -sf /etc/init.d/force-fsck /etc/rc6.d/S61force-fschk
