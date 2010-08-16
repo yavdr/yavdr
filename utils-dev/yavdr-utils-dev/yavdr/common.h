693be59adf04cae368b693bbad83a8431d4502aa
hotzenplotz5 <hotzenplotz5@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-08-16 20:29:33 +0000

    utils-dev : utils development headers
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@1699 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

---
+++ utils-dev/yavdr-utils-dev/yavdr/common.h
@@ -0,0 +1,12 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#define YAVDRDB			"/var/lib/yavdrdb.hdf"
+//#define YAVDRDB_LCK		"/var/lock/yavdrdb.hdf.LCK"
+#define TEMPLATEPATH		"/usr/share/yavdr/templates"
+#define TEMPLATECUSTOMPATH	"/etc/yavdr/templates_custom"
+#define YAVDREVENTS		"/usr/share/yavdr/events"
+#define YAVDRACTIONS		"/usr/share/yavdr/events/actions"
+#define PRCTMPLBACKDIR		"/var/cache/yavdr/process-template-backup"
+//#define YAVDRNAV	"/usr/share/yavdr/navigation.hdf" not needed for new web-interface
+#endif // __COMMON_H__
