55e6c7db00eeb1dbc9036827c8f72991c069f326
hotzenplotz5 <hotzenplotz5@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-08-19 16:56:57 +0000

    sundtek.js not finished (restart vdr button, language labels, etc
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@1733 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

---
+++ yavdr-installer/sundtek/yavdr-installer-sundtek/yavdrweb-ng/static/config_tabs/sundtek.js
@@ -0,0 +1,72 @@
+function getVDRSundtekForm(){
+    var myform = new Ext.FormPanel({
+        frame: false,
+        plain: false,
+        border: false,
+        bodyStyle:'padding:5px 5px 0',
+        labelWidth: 150,
+        //defaultType: 'textfield',
+        buttonAlign: 'left',
+        items: [{
+            id: 'sundtek_radio_group',
+            name: 'sundtek',
+            xtype: 'checkboxgroup',
+            fieldLabel: getLL("sundtek.label"),
+            columns: 2,
+            items: [
+                {id: 'dvbc', boxLabel: 'DVB-C', name: 'values', inputValue: 'dvbc'},
+                {id: 'dvbt', boxLabel: 'DVB-T', name: 'values', inputValue: 'dvbt'}
+            ]
+        },{
+            html: '<br/><br/>' + getLL("sundtek.help")
+        }]
+    });
+
+    var submit = myform.addButton({
+        text: getLL("sundtek.button_label"),
+        icon: '/ext/resources/images/default/grid/refresh.gif',
+        //formBind: true,
+        //scope: this,
+        handler: function() {
+            myform.form.submit({
+                url: 'set_dvb',
+                timeout: 30, //wait 30 seconds before telling it failed
+                waitMsg: getLL("sundtek.submit.waitmsg"),
+                waitTitle: getLL("standardform.messagebox_caption.wait"),
+                scope:this,
+                success: function (form, action) {
+                    Ext.MessageBox.alert( getLL("standardform.messagebox_caption.message"), getLL("sundtek.submit.success") );
+                },
+                failure:function(form, action) {
+                    Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), getLL("sundtek.submit.failure") );
+                }
+            })
+        }
+    });
+    
+    Ext.Ajax.request({
+        url: 'get_dvb',
+        timeout: 3000,
+        method: 'GET',
+        scope: myform,
+        success: function(xhr) {
+            //alert('Response is "' + xhr.responseText + '"');
+            var currentSundtek = "";
+            try {
+                currentSundtek = xhr.responseText;
+            }
+            catch (err) {
+                Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), 'Could not recognize current sound setting.');
+            }
+            if (currentSundtek == "dvbc" || currentSundtek == "dvbt"){
+                var rButton = this.getComponent('Sundtek_radio_group');
+                if (rButton)
+                    rButton.setValue( currentSundtek );
+                else
+                    Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), 'Could not find Sundtek radiobutton group.');
+            }
+        }
+    });
+    
+    return myform;
+}
