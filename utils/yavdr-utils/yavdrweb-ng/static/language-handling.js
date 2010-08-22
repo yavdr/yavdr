b01928ca134484c5cd24c3863bc25a6c705ed307
v_r <v_r@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-08-22 13:32:40 +0000

    first try handling wfe pugin locales.
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@1760 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/yavdr-utils/yavdrweb-ng/static/language-handling.js
+++ utils/yavdr-utils/yavdrweb-ng/static/language-handling.js
@@ -32,3 +32,12 @@ function getLL( param ){
     }
     return label;
 }
+
+function applyLL(o, l) {
+    if(o && c && typeof c == 'object'){
+        for(var p in c){
+            o[p] = c[p];
+        }
+    }
+    return o;
+}
---
+++ yavdr-installer/sundtek/yavdr-installer-sundtek/yavdrweb-ng/static/locale/sundtek_en.js
@@ -0,0 +1,17 @@
+
+applyLL(locale_en,{
+    sundtek: {
+        label: 'sundtek DVB mode',
+        menutab: {
+            title: '',
+            tabtip: '',
+            panel_title: ''
+        },
+        button_label: '',
+        submit: {
+            waitmsg: '',
+            success: ''
+        }
+        help: ''
+    }
+})
