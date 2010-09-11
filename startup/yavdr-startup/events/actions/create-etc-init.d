ef3ec8618b4c8a80ffef450cd81491201fa4d368
steffen_b <steffen_b@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-09-11 18:24:53 +0000

    change shutdown back in vdr.conf and create init script to wait for vdr fixes #100 , also delays rsyslogd to a very late point
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@1900 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- startup/yavdr-startup/events/actions/create-etc-init.d
+++ startup/yavdr-startup/events/actions/create-etc-init.d
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 process-template --mode 775 /etc/init.d/force-fsck
+process-template --mode 775 /etc/init.d/yavdr-shutdown
 
 # Symlinks erstellen, die beim Shutdown ausgeführt werden:
 ln -sf /etc/init.d/force-fsck /etc/rc0.d/S61force-fschk
 ln -sf /etc/init.d/force-fsck /etc/rc6.d/S61force-fschk
+ln -sf /etc/init.d/yavdr-shutdown /etc/rc0.d/S59yavdr-shutdown
+ln -sf /etc/init.d/yavdr-shutdown /etc/rc6.d/S59yavdr-shutdown
+
 rm -f /etc/rc6.d/S61force-fchk
 rm -f /etc/rc0.d/S61force-fchk
