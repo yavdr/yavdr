0be9362470693bca0a5c87d58ae352a5a3acaae0
v_r <v_r@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-09-17 19:13:44 +0000

    fix lifeguard tpl activate lg checkboxes by default
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@1941 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/yavdr-utils/dvb-test/dvb-test.c
+++ utils/yavdr-utils/dvb-test/dvb-test.c
@@ -10,15 +10,14 @@
 #include <linux/dvb/version.h>
 #include <sys/ioctl.h>
 
-
 struct tDvbParameterMap {
-  int userValue;
-  int driverValue;
-  const char *userString;
+	int userValue;
+	int driverValue;
+	const char *userString;
 };
 
-
 static int wait = 0;
+static int syslog = 0;
 
 const struct tDvbParameterMap ModulationValues[] = { { 16, QAM_16, "QAM16" }, {
 		32, QAM_32, "QAM32" }, { 64, QAM_64, "QAM64" }, { 128, QAM_128,
@@ -46,6 +45,25 @@ const char *MapToUserString(int Value, const struct tDvbParameterMap *Map) {
 		return Map[n].userString;
 	return "???";
 }
+/*
+ int test() {
+ cString FileName = DvbName(DEV_DVB_FRONTEND, Adapter, Frontend);
+ if (access(FileName, F_OK) == 0) {
+ int f = open(FileName, O_RDONLY);
+ if (f >= 0) {
+ close(f);
+ return true;
+ } else if (errno != ENODEV && errno != EINVAL) {
+ LOG_ERROR_STR("open");
+ LOG_ERROR_STR(*FileName);
+ }
+ } else if (errno != ENOENT) {
+ LOG_ERROR_STR("access");
+ LOG_ERROR_STR(*FileName);
+ }
+ return false;
+ }
+ */
 
 int main(int argc, char *argv[]) {
 	int ret = 0;
@@ -58,107 +76,109 @@ int main(int argc, char *argv[]) {
 	while (1) {
 		int longindex = 0;
 
-		static struct option longopts[] = { { "wait", 0, &wait, 1 }, { 0, 0, 0,
-				0 } };
+		static struct option longopts[] = { { "wait", 0, &wait, 1 }, {
+				"syslog", 0, &syslog, 1 }, { 0, 0, 0, 0 } };
 
 		if ((c = getopt_long(argc, argv, "", longopts, &longindex)) == -1)
 			break;
 
 	}
 	if (optind < argc) {
-		//setlocale(LC_ALL, "");
-		//bindtextdomain("yavdr", "/usr/share/yavdr/locale");
-		//textdomain("yavdr");
-
 		for (i = optind; i < argc; i++) {
 			printf("checking : %s\n", argv[i]);
 
 			do {
-				int fd_frontend = open(argv[i], O_RDWR | O_NONBLOCK);
+				if (access(argv[i], F_OK) == 0) {
+					printf("start open");
+					int fd_frontend = open(argv[i], O_RDONLY);
+					printf("end open");
+					if (fd_frontend >= 0) {
+						if (ioctl(fd_frontend, FE_GET_INFO, &frontendInfo) >= 0) {
+							switch (frontendInfo.type) {
+							case FE_QPSK:
+								frontendType = (frontendInfo.caps
+										& FE_CAN_2G_MODULATION) ? SYS_DVBS2
+										: SYS_DVBS;
+								break;
+							case FE_OFDM:
+								frontendType = SYS_DVBT;
+								break;
+							case FE_QAM:
+								frontendType = SYS_DVBC_ANNEX_AC;
+								break;
+							case FE_ATSC:
+								frontendType = SYS_ATSC;
+								break;
+							default:
+								fprintf(
+										stderr,
+										"ERROR: unknown frontend type %d on frontend %s",
+										frontendInfo.type, argv[i]);
+							}
+							if (frontendType != SYS_UNDEFINED) {
+								numProvidedSystems++;
+								if (frontendType == SYS_DVBS2)
+									numProvidedSystems++;
+								char Modulations[64];
+								char *p = Modulations;
+								if (frontendInfo.caps & FE_CAN_QPSK) {
+									numProvidedSystems++;
+									p += sprintf(p, ",%s", MapToUserString(
+											QPSK, ModulationValues));
+								}
+								if (frontendInfo.caps & FE_CAN_QAM_16) {
+									numProvidedSystems++;
+									p += sprintf(p, ",%s", MapToUserString(
+											QAM_16, ModulationValues));
+								}
+								if (frontendInfo.caps & FE_CAN_QAM_32) {
+									numProvidedSystems++;
+									p += sprintf(p, ",%s", MapToUserString(
+											QAM_32, ModulationValues));
+								}
+								if (frontendInfo.caps & FE_CAN_QAM_64) {
+									numProvidedSystems++;
+									p += sprintf(p, ",%s", MapToUserString(
+											QAM_64, ModulationValues));
+								}
+								if (frontendInfo.caps & FE_CAN_QAM_128) {
+									numProvidedSystems++;
+									p += sprintf(p, ",%s", MapToUserString(
+											QAM_128, ModulationValues));
+								}
+								if (frontendInfo.caps & FE_CAN_QAM_256) {
+									numProvidedSystems++;
+									p += sprintf(p, ",%s", MapToUserString(
+											QAM_256, ModulationValues));
+								}
+								if (frontendInfo.caps & FE_CAN_8VSB) {
+									numProvidedSystems++;
+									p += sprintf(p, ",%s", MapToUserString(
+											VSB_8, ModulationValues));
+								}
+								if (frontendInfo.caps & FE_CAN_16VSB) {
+									numProvidedSystems++;
+									p += sprintf(p, ",%s", MapToUserString(
+											VSB_16, ModulationValues));
+								}
+								//if (frontendInfo.caps & FE_CAN_TURBO_FEC){numProvidedSystems++; p += sprintf(p, ",%s", "TURBO_FEC"); }
+								if (p != Modulations)
+									p = Modulations + 1; // skips first ','
+								else
+									p = (char *) "unknown modulations";
+								printf(
+										"frontend %s provides %s with %s (\"%s\")\n",
+										argv[i], DeliverySystems[frontendType],
+										p, frontendInfo.name);
 
-				if (fd_frontend >= 0) {
-					if (ioctl(fd_frontend, FE_GET_INFO, &frontendInfo) >= 0) {
-						switch (frontendInfo.type) {
-						case FE_QPSK:
-							frontendType = (frontendInfo.caps
-									& FE_CAN_2G_MODULATION) ? SYS_DVBS2
-									: SYS_DVBS;
-							break;
-						case FE_OFDM:
-							frontendType = SYS_DVBT;
-							break;
-						case FE_QAM:
-							frontendType = SYS_DVBC_ANNEX_AC;
-							break;
-						case FE_ATSC:
-							frontendType = SYS_ATSC;
-							break;
-						default:
-							fprintf(
-									stderr,
-									"ERROR: unknown frontend type %d on frontend %s",
-									frontendInfo.type, argv[i]);
+							}
 						}
-						if (frontendType != SYS_UNDEFINED) {
-							numProvidedSystems++;
-							if (frontendType == SYS_DVBS2)
-								numProvidedSystems++;
-							char Modulations[64];
-							char *p = Modulations;
-							if (frontendInfo.caps & FE_CAN_QPSK) {
-								numProvidedSystems++;
-								p += sprintf(p, ",%s", MapToUserString(QPSK,
-										ModulationValues));
-							}
-							if (frontendInfo.caps & FE_CAN_QAM_16) {
-								numProvidedSystems++;
-								p += sprintf(p, ",%s", MapToUserString(QAM_16,
-										ModulationValues));
-							}
-							if (frontendInfo.caps & FE_CAN_QAM_32) {
-								numProvidedSystems++;
-								p += sprintf(p, ",%s", MapToUserString(QAM_32,
-										ModulationValues));
-							}
-							if (frontendInfo.caps & FE_CAN_QAM_64) {
-								numProvidedSystems++;
-								p += sprintf(p, ",%s", MapToUserString(QAM_64,
-										ModulationValues));
-							}
-							if (frontendInfo.caps & FE_CAN_QAM_128) {
-								numProvidedSystems++;
-								p += sprintf(p, ",%s", MapToUserString(QAM_128,
-										ModulationValues));
-							}
-							if (frontendInfo.caps & FE_CAN_QAM_256) {
-								numProvidedSystems++;
-								p += sprintf(p, ",%s", MapToUserString(QAM_256,
-										ModulationValues));
-							}
-							if (frontendInfo.caps & FE_CAN_8VSB) {
-								numProvidedSystems++;
-								p += sprintf(p, ",%s", MapToUserString(VSB_8,
-										ModulationValues));
-							}
-							if (frontendInfo.caps & FE_CAN_16VSB) {
-								numProvidedSystems++;
-								p += sprintf(p, ",%s", MapToUserString(VSB_16,
-										ModulationValues));
-							}
-							//if (frontendInfo.caps & FE_CAN_TURBO_FEC){numProvidedSystems++; p += sprintf(p, ",%s", "TURBO_FEC"); }
-							if (p != Modulations)
-								p = Modulations + 1; // skips first ','
-							else
-								p = (char *) "unknown modulations";
-							printf("frontend %s provides %s with %s (\"%s\")\n",
-									argv[i], DeliverySystems[frontendType], p,
-									frontendInfo.name);
-
-						}
+						printf("frontend %s is ready\n", argv[i]);
+					} else {
+						printf("frontend %s not ready\n", argv[i]);
 					}
-
 				} else {
-
+					printf("access to frontend %s denied\n", argv[i]);
 				}
 			} while (wait == 1);
 		}
--- utils/yavdr-utils/yavdrweb-ng/static/config_tabs/lifeguard.js
+++ utils/yavdr-utils/yavdrweb-ng/static/config_tabs/lifeguard.js
@@ -1,86 +1,125 @@
-function getVDRLifeguardForm(){
-    var myform = new Ext.FormPanel({
-        frame: false,
-        plain: false,
-        border: false,
-        bodyStyle:'padding:5px 5px 0',
-        labelWidth: 150,
-        //defaultType: 'textfield',
-        buttonAlign: 'left',
-        items: [{
-            id: 'lifeguard_radio_group',
-            name: 'lifeguard',
-            xtype: 'checkboxgroup',
-            fieldLabel: getLL("lifeguard.label"),
-            columns: 2,
-            items: [
-                {id: 'lifeguard_aptitude', boxLabel: 'Aptitude', name: 'values', inputValue: 'aptitude'},
-                {id: 'lifeguard_ssh', boxLabel: 'SSH', name: 'values', inputValue: 'ssh'},
-                {id: 'lifeguard_nfs', boxLabel: 'NFS', name: 'values', inputValue: 'nfs'},
-		        {id: 'lifeguard_ftp', boxLabel: 'FTP', name: 'values', inputValue: 'ftp'},
-                {id: 'lifeguard_smb', boxLabel: 'SMB', name: 'values', inputValue: 'smb'},		
-                {id: 'lifeguard_xbmc', boxLabel: 'XBMC', name: 'values', inputValue: 'xbmc'}
-            ]
-        },{
-            html: '<br/><br/>' + getLL("lifeguard.help")
-        }]
-    });
+function getVDRLifeguardForm() {
+	var myform = new Ext.FormPanel( {
+		frame : false,
+		plain : false,
+		border : false,
+		bodyStyle : 'padding:5px 5px 0',
+		labelWidth : 150,
+		// defaultType: 'textfield',
+		buttonAlign : 'left',
+		items : [ {
+			id : 'lifeguard_radio_group',
+			name : 'lifeguard',
+			xtype : 'checkboxgroup',
+			fieldLabel : getLL("lifeguard.label"),
+			columns : 2,
+			items : [ {
+				id : 'lifeguard_aptitude',
+				boxLabel : 'Aptitude',
+				name : 'values',
+				inputValue : 'aptitude'
+			}, {
+				id : 'lifeguard_ssh',
+				boxLabel : 'SSH',
+				name : 'values',
+				inputValue : 'ssh'
+			}, {
+				id : 'lifeguard_nfs',
+				boxLabel : 'NFS',
+				name : 'values',
+				inputValue : 'nfs'
+			}, {
+				id : 'lifeguard_ftp',
+				boxLabel : 'FTP',
+				name : 'values',
+				inputValue : 'ftp'
+			}, {
+				id : 'lifeguard_smb',
+				boxLabel : 'SMB',
+				name : 'values',
+				inputValue : 'smb'
+			}, {
+				id : 'lifeguard_xbmc',
+				boxLabel : 'XBMC',
+				name : 'values',
+				inputValue : 'xbmc'
+			} ]
+		}, {
+			html : '<br/><br/>' + getLL("lifeguard.help")
+		} ]
+	});
 
-    var submit = myform.addButton({
-        text: getLL("lifeguard.button_label"),
-        icon: '/ext/resources/images/default/grid/refresh.gif',
-        //formBind: true,
-        //scope: this,
-        handler: function() {
-            myform.form.submit({
-                url: 'set_signal?signal=change-lifeguard',
-                timeout: 30, //wait 30 seconds before telling it failed
-                waitMsg: getLL("lifeguard.submit.waitmsg"),
-                waitTitle: getLL("standardform.messagebox_caption.wait"),
-                scope:this,
-                success: function (form, action) {
-                    Ext.MessageBox.alert( getLL("standardform.messagebox_caption.message"), getLL("lifeguard.submit.success") );
-                },
-                failure:function(form, action) {
-                    Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), getLL("lifeguard.submit.failure") );
-                }
-            })
-        }
-    });
-    
-    Ext.Ajax.request({
-        url: 'get_hdf_value?hdftree=vdr.plugin.lifeguard.enable',
-        timeout: 3000,
-        method: 'GET',
-        scope: myform,
-        success: function(xhr) {
-            //alert('Response is "' + xhr.responseText + '"');
-            try {
-            	var currentLifeguard = Ext.util.JSON.decode( xhr.responseText );
-            	for(var i=0; i < currentLifeguard.length; i++) {
-                	var rButton = Ext.getCmp('lifeguard_'+currentLifeguard[i]);
-                    if (rButton)
-                        rButton.setValue( true );
-                    else
-                        Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), 'Could not find lifeguard radiobutton group.');
-                }
-            }
-            catch (err) {
-                Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), 'Could not recognize current sound setting.');
-            }
+	var submit = myform.addButton( {
+		text : getLL("lifeguard.button_label"),
+		icon : '/ext/resources/images/default/grid/refresh.gif',
+		// formBind: true,
+		// scope: this,
+		handler : function() {
+			myform.form.submit( {
+				url : 'set_signal?signal=change-lifeguard',
+				timeout : 30, // wait 30 seconds before telling it failed
+				waitMsg : getLL("lifeguard.submit.waitmsg"),
+				waitTitle : getLL("standardform.messagebox_caption.wait"),
+				scope : this,
+				success : function(form, action) {
+					Ext.MessageBox.alert(
+							getLL("standardform.messagebox_caption.message"),
+							getLL("lifeguard.submit.success"));
+				},
+				failure : function(form, action) {
+					Ext.MessageBox.alert(
+							getLL("standardform.messagebox_caption.error"),
+							getLL("lifeguard.submit.failure"));
+				}
+			})
+		}
+	});
 
-        }
-    });
-    
-    return myform;
+	Ext.Ajax.request( {
+		url : 'get_hdf_value?hdftree=vdr.plugin.lifeguard.enable',
+		timeout : 3000,
+		method : 'GET',
+		scope : myform,
+		success : function(xhr) {
+			// alert('Response is "' + xhr.responseText + '"');
+		try {
+			var currentLifeguard = Ext.util.JSON.decode(xhr.responseText);
+			if (currentLifeguard.length > 0) {
+				for ( var i = 0; i < currentLifeguard.length; i++) {
+					var rButton = Ext
+							.getCmp('lifeguard_' + currentLifeguard[i]);
+					if (rButton)
+						rButton.setValue(true);
+					else
+						Ext.MessageBox.alert(
+								getLL("standardform.messagebox_caption.error"),
+								'Could not find lifeguard radiobutton group.');
+				}
+			} else {
+				var group = this.getComponent('lifeguard_radio_group');
+				Ext.each(group.items.items, function(e, index) {
+					e.setValue(true);
+				});
+			}
+		} catch (err) {
+			Ext.MessageBox.alert(
+					getLL("standardform.messagebox_caption.error"),
+					'Could not recognize current sound setting.');
+		}
+
+	}
+	});
+
+	return myform;
 }
 
 Ext.onReady(function() {
-    if (yavdrwebGlobalInfo.devmode == "1"){
-        YaVDRMenuManager
-            .addGroupPanelSection({section: "development"})
-                .addGroupPanelTab({
-                    section: "lifeguard",
-                    items:   getVDRLifeguardForm});
-    }
+	if (yavdrwebGlobalInfo.devmode == "1") {
+		YaVDRMenuManager.addGroupPanelSection( {
+			section : "development"
+		}).addGroupPanelTab( {
+			section : "lifeguard",
+			items : getVDRLifeguardForm
+		});
+	}
 });
