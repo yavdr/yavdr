6f3221551e8a47d7ca8a823b84051c10cf7e6d96
traxanos <traxanos@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-10-20 20:54:38 +0000

    use global css for duplicate styles; add dpkg.js; cleanup packages.js
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@2124 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/yavdr-utils/yavdrweb-ng/static/config_tabs/packages.js
+++ utils/yavdr-utils/yavdrweb-ng/static/config_tabs/packages.js
@@ -1,169 +1,3 @@
-// Installer Component
-YaVDR.DPKG = Ext.extend(Ext.Window, {
-  callback: null,
-  store: null,
-  command: 'update',
-  title: 'yaVDR Paket Installer',
-  width: 600,
-  height: 450,
-  resizable: true,
-  maximizable: true,
-  closable: false,
-  modal: true,
-  layout: 'fit',
-  id: 'dpkg',
-  initComponent: function() {
-
-    if (this.command == 'install' || this.command == 'remove') {
-      var url = 'dpkg?command=' + this.command + '&package=' + this.package + '&ts=' + ((new Date()).getTime());
-    } else {
-      var url = 'dpkg?command=' + this.command + '&ts=' + ((new Date()).getTime());
-    }
-        
-    this.iframe = new Ext.ux.ManagedIFrame.Panel({
-      border: false,
-      defaultSrc : url
-    });
-
-    this.items = [this.iframe];
-
-    this.closeButton = new Ext.Button({
-      disabled:true,
-      scope: this,
-      text: 'Close',
-      handler: function() {
-        this.close();
-        if (typeof this.callback == 'function') {
-          if (this.scope) {
-            this.callback.call(this.scope);
-          } else {
-            this.callback();
-          }
-        }
-      }
-    });
-    
-    this.buttons = [this.closeButton];
-
-    YaVDR.DPKG.superclass.initComponent.call(this);
-    
-    this.iframe.on('domready', this.activateCloseButton, this, { single: true });
-  },
-  activateCloseButton: function() {
-    this.closeButton.enable();
-  }
-});
-
-// Public API
-Ext.apply(YaVDR.DPKG, {
-  update: function(callback, scope) {
-    (new YaVDR.DPKG({
-      scope: scope,
-      command: 'update',
-      callback: callback
-    })).show();
-  },
-  autoremove: function(callback, scope) {
-    (new YaVDR.DPKG({
-      scope: scope,
-      command: 'autoremove',
-      callback: callback
-    })).show();
-  },
-  install: function(package, callback, scope) {
-    Ext.Msg.show({
-      title:'Install package?',
-      msg: 'Would you like to install package "' + Ext.util.Format.htmlEncode(package) + '"?',
-      buttons: Ext.Msg.YESNO,
-      animEl: 'elId',
-      scope: this,
-      icon: Ext.MessageBox.QUESTION,
-      fn: function(button) {
-        if (button == 'yes') {
-          (new YaVDR.DPKG({
-            scope: scope,
-            package: package,
-            command: 'install',
-            callback: callback
-          })).show();
-        }
-      }
-    });
-  },
-  remove: function(package, callback, scope) {
-    Ext.Msg.show({
-      title:'Install package?',
-      msg: 'Would you like to remove package "' + Ext.util.Format.htmlEncode(package) + '"?',
-      buttons: Ext.Msg.YESNO,
-      animEl: 'elId',
-      scope: this,
-      icon: Ext.MessageBox.QUESTION,
-      fn: function(button) {
-        if (button == 'yes') {
-          (new YaVDR.DPKG({
-            scope: scope,
-            package: package,
-            command: 'remove',
-            callback: callback
-          })).show();
-        }
-      }
-    });
-  },
-  enable: function(package, callback, scope) {
-    Ext.Msg.show({
-      title:'enable plugin?',
-      msg: 'Would you like to enable "' + Ext.util.Format.htmlEncode(package) + '"?',
-      buttons: Ext.Msg.YESNO,
-      fn: function(buttonId, text, opt) {
-        if (buttonId == "yes") {
-          Ext.Ajax.request({
-            url: 'set_signal?signal=change-plugin&signal_params=enable ' + package,
-            waitMsg: getLL("nvidia.submit.waitmsg"),
-            timeout: 3000,
-            method: 'GET',
-            success: function(xhr) {
-              callback.call(scope, true);
-            },
-            failure: function() {
-              alert('fatal error: disable plugin failed');
-              callback.call(scope, false);
-            }
-          });
-        }
-      },
-      animEl: 'elId',
-      icon: Ext.MessageBox.QUESTION
-    });
-  },
-  disable: function(package, callback, scope) {
-    Ext.Msg.show({
-      title:'disable plugin?',
-      msg: 'Would you like to disable "' + Ext.util.Format.htmlEncode(package) + '"?',
-      buttons: Ext.Msg.YESNO,
-      fn: function(button) {
-        if (button == "yes") {
-          Ext.Ajax.request({
-            url: 'set_signal?signal=change-plugin&signal_params=disable ' + package,
-            waitMsg: getLL("nvidia.submit.waitmsg"),
-            timeout: 3000,
-            method: 'GET',
-            success: function(xhr) {
-              callback.call(scope, true);
-            },
-            failure: function() {
-              alert('fatal error: disable plugin failed');
-              callback.call(scope, false);
-            }
-          });
-        }
-      },
-      animEl: 'elId',
-      icon: Ext.MessageBox.QUESTION
-    });
-  }
-});
-
 YaVDR.Packages = Ext.extend(YaVDR.BasePanel, {
   layout: 'border',
   buttonAlign: 'left',
@@ -284,7 +118,7 @@ YaVDR.Packages = Ext.extend(YaVDR.BasePanel, {
     this.detailPanel = new Ext.Panel({
       region: 'south',
       anchor: '100%',
-      style: 'border: 1px solid #99BBE8',
+      cls: 'frame-panel-border',
       autoScroll: true,
       bodyStyle: {
         background: '#ffffff',
@@ -308,7 +142,7 @@ YaVDR.Packages = Ext.extend(YaVDR.BasePanel, {
       region: 'center',
       anchor: '100%',
       loadMask: true,
-      style: 'border: 1px solid #99BBE8',
+      cls: 'frame-panel-border',
       margins: '0 0 5 0',
       viewConfig: {
         forceFit: true
