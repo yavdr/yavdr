d6be9952f1ee1931ed5f77aecfb788c60b02a5c5
traxanos <traxanos@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-10-21 17:39:51 +0000

    fix: reload page only user click yes; fix reload call; refactor web-frontend
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@2142 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/yavdr-utils/yavdrweb-ng/static/config_tabs/web-frontend.js
+++ utils/yavdr-utils/yavdrweb-ng/static/config_tabs/web-frontend.js
@@ -1,3 +1,109 @@
+YaVDR.WebFrontend = Ext.extend(YaVDR.BaseFormPanel, {
+  labelWidth: 300,
+  initComponent: function() {
+    
+    this.store = new Ext.data.JsonStore({
+      fields: [
+        { name: 'key' },
+        { name: 'title' }
+      ],
+
+      data: [
+        {
+          key: 'zh',
+          title: 'Chinese'
+        },
+        {
+          key: 'nl',
+          title: 'Dutch'
+        },
+        {
+          key: 'en',
+          title: 'English'
+        },
+        {
+          key: 'fr',
+          title: 'French'
+        },
+        {
+          key: 'de',
+          title: 'German'
+        },
+        {
+          key: 'it',
+          title: 'Italian'
+        },
+        {
+          key: 'pt',
+          title: 'Portugese'
+        }
+      ]
+
+    });
+    
+    this.languageTpl = new Ext.XTemplate(
+      '<tpl for=".">',
+        '<div class="selection-wrap selectable" id="language-selection-{key}">',
+          '<div class="title">{title}</div>',
+        '</div>',
+      '</tpl>'
+    );
+    
+    this.languageSelectionHidden = new Ext.form.Hidden({
+      name: 'value',
+      value: 'english'
+    });
+    
+    this.languageSelectiorView = new YaVDR.SelectionList({
+      hiddenField: this.languageSelectionHidden,
+      fieldLabel: getLL("webfrontend.label"),
+      tpl: this.languageTpl,
+      store: this.store
+    });
+    
+    this.items = [
+      this.languageSelectionHidden,
+      this.languageSelectiorView
+    ]
+    
+    this.tbar = [
+      {
+        scope: this,
+        itemId: 'save',
+        text: 'Auswahl übernehmen',
+        icon: '/static/images/icons/save.png',
+        handler: this.saveSelection
+      }
+    ];
+    
+    YaVDR.WebFrontend.superclass.initComponent.call(this);
+    
+    this.on('render', this.loadSelection, this, { single: true });
+  },
+  saveSelection: function() {
+    this.getForm().submit({
+      url: 'set_hdf_value?key=webfrontend.language',
+      waitMsg: getLL("webfrontend.submit.waitmsg"),
+      waitTitle: getLL("standardform.messagebox_caption.wait"),
+      scope:this,
+      success: function (form, action) {
+        Ext.MessageBox.confirm( getLL("standardform.messagebox_caption.message"), getLL("webfrontend.submit.success"), function(x){
+          if(x == 'yes') window.location.reload();
+        });
+      },
+      failure:function(form, action) {
+          Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), getLL("webfrontend.submit.failure") );
+      }
+    });
+  },
+  loadSelection: function() {
+    YaVDR.getHdfValue('webfrontend.language', function(value) {
+      this.languageSelectiorView.select("language-selection-" + value);
+    }, this);
+  }
+});
+
+
 function getWebFrontendForm(){
     var myform = new Ext.FormPanel({
         frame: false,
@@ -87,5 +193,6 @@ Ext.onReady(function() {
         .addGroupPanelSection({section: "system"})
             .addGroupPanelTab({
                 section: "webfrontend",
-                items:   getWebFrontendForm});
+                layout: 'fit',
+                items:   function() { return new YaVDR.WebFrontend }});
 });
