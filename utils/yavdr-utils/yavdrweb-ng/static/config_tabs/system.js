37a5de2d707e8f1a3ed2f4fdf22a5649ccb9e68e
traxanos <traxanos@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-10-22 16:48:19 +0000

    refactor system
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@2155 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/yavdr-utils/yavdrweb-ng/static/config_tabs/system.js
+++ utils/yavdr-utils/yavdrweb-ng/static/config_tabs/system.js
@@ -1,96 +1,100 @@
-function getSystemForm(){
-    var myform = new Ext.FormPanel({
-        frame: false,
-        plain: false,
-        border: false,
-        bodyStyle:'padding:5px 5px 0',
-        labelWidth: 130,
-        defaults: {width: 500},
-        defaultType: 'textfield',
-        buttonAlign: 'left'
-    });
-/*
-    var submit_vdr_stop = myform.addButton({
+YaVDR.Commands = Ext.extend(YaVDR.BaseFormPanel, {
+  initComponent: function() {
+    
+    this.tbar = [
+      {
+        icon: '/static/images/icons/system_stop.png',
         text: 'VDR stoppen',
-        icon: '/ext/resources/images/default/grid/refresh.gif',
-        handler: function() {
-            myform.form.submit({
-                url: 'set_signal?signal=stop_vdr',
-                waitMsg:'Das Signal zum Stoppen des VDR wird abgesetzt.',
-                scope:this,
-                success: function (form, action) {
-                    Ext.MessageBox.alert('Message', 'Der VDR wird nun gestoppt.');
-                },
-                failure:function(form, action) {
-                    Ext.MessageBox.alert('Message', 'Fehler beim Absetzen des Signals. Bitte noch einmal versuchen.');
-                }
-            })
-        }
-    });
-    */
-    var submit_vdr_restart = myform.addButton({
+        scope: this,
+        handler: this.stopVdr
+      },
+      {
+        icon: '/static/images/icons/system_vdr_restart.png',
         text: getLL("system.vdr_restart.label"),
-        icon: '/ext/resources/images/default/grid/refresh.gif',
-        handler: function() {
-            myform.form.submit({
-                url: 'set_signal?signal=restart-vdr',
-                waitMsg: getLL("system.vdr_restart.submit.waitmsg"),
-                waitTitle: getLL("standardform.messagebox_caption.wait"),
-                scope:this,
-                success: function (form, action) {
-                    Ext.MessageBox.alert( getLL("standardform.messagebox_caption.message"), getLL("system.vdr_restart.submit.success") );
-                },
-                failure:function(form, action) {
-                    Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), getLL("system.vdr_restart.submit.failure") );
-                }
-            })
-        }
-    });
-
-    var submit_kill_xbmc = myform.addButton({
+        scope: this,
+        handler: this.restartVdr
+      }, 
+      {
+        icon: '/static/images/icons/system_kill_xbmc.png',
         text: getLL("system.kill_xbmc.label"),
-        //icon: '/ext/resources/images/default/grid/refresh.gif',
-        handler: function() {
-            myform.form.submit({
-                url: 'set_signal?signal=kill-xbmc',
-                waitMsg: getLL("system.kill_xbmc.submit.waitmsg"),
-                waitTitle: getLL("standardform.messagebox_caption.wait"),
-                scope:this,
-                success: function (form, action) {
-                    Ext.MessageBox.alert( getLL("standardform.messagebox_caption.message"), getLL("system.kill_xbmc.submit.success") );
-                },
-                failure:function(form, action) {
-                    Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), getLL("system.kill_xbmc.submit.failure") );
-                }
-            })
-        }
-    });
-
-    var submit_reboot = myform.addButton({
+        scope: this,
+        handler: this.killXBMC
+      },
+      {
+        icon: '/static/images/icons/system_restart.png',
         text: getLL("system.system_restart.label"),
-        icon: '/ext/resources/images/default/grid/refresh.gif',
-        handler: function() {
-            myform.form.submit({
-                url: 'set_signal?signal=reboot',
-                waitMsg: getLL("system.system_restart.submit.waitmsg"),
-                waitTitle: getLL("standardform.messagebox_caption.wait"),
-                scope:this,
-                success: function (form, action) {
-                    Ext.MessageBox.alert( getLL("standardform.messagebox_caption.message"), getLL("system.system_restart.submit.success") );
-                },
-                failure:function(form, action) {
-                    Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), getLL("system.system_restart.submit.failure") );
-                }
-            })
-        }
-    });
-    return myform;
-}
+        scope: this,
+        handler: this.restartSystem
+      },
+    ];
+    
+    this.html = "";
+    
+    YaVDR.Commands.superclass.initComponent.call(this);
+  },
+  stopVdr: function() {
+    this.getForm().submit({
+      url: 'set_signal?signal=stop_vdr',
+      waitMsg:'Das Signal zum Stoppen des VDR wird abgesetzt.',
+      scope: this,
+      success: function (form, action) {
+        Ext.MessageBox.alert('Message', 'Der VDR wird nun gestoppt.');
+      },
+      failure:function(form, action) {
+        Ext.MessageBox.alert('Message', 'Fehler beim Absetzen des Signals. Bitte noch einmal versuchen.');
+      }
+    })
+  },
+  restartVdr: function() {
+    this.getForm().submit({
+      url: 'set_signal?signal=restart-vdr',
+      waitMsg: getLL("system.vdr_restart.submit.waitmsg"),
+      waitTitle: getLL("standardform.messagebox_caption.wait"),
+      scope: this,
+      success: function (form, action) {
+        Ext.MessageBox.alert( getLL("standardform.messagebox_caption.message"), getLL("system.vdr_restart.submit.success") );
+      },
+      failure:function(form, action) {
+        Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), getLL("system.vdr_restart.submit.failure") );
+      }
+    })
+  },
+  killXBMC: function() {
+    this.getForm().submit({
+      url: 'set_signal?signal=kill-xbmc',
+      waitMsg: getLL("system.kill_xbmc.submit.waitmsg"),
+      waitTitle: getLL("standardform.messagebox_caption.wait"),
+      scope: this,
+      success: function (form, action) {
+        Ext.MessageBox.alert( getLL("standardform.messagebox_caption.message"), getLL("system.kill_xbmc.submit.success") );
+      },
+      failure:function(form, action) {
+        Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), getLL("system.kill_xbmc.submit.failure") );
+      }
+    })
+  },
+  restartSystem: function() {
+    this.getForm().submit({
+      url: 'set_signal?signal=reboot',
+      waitMsg: getLL("system.system_restart.submit.waitmsg"),
+      waitTitle: getLL("standardform.messagebox_caption.wait"),
+      scope: this,
+      success: function (form, action) {
+        Ext.MessageBox.alert( getLL("standardform.messagebox_caption.message"), getLL("system.system_restart.submit.success") );
+      },
+      failure:function(form, action) {
+        Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), getLL("system.system_restart.submit.failure") );
+      }
+    })
+  }
+  
+});
 
 Ext.onReady(function() {
     YaVDRMenuManager
         .addGroupPanelSection({section: "system"})
             .addGroupPanelTab({
                 section: "system",
-                items:   getSystemForm});
+                layout: 'fit',
+                items:   function() { return new YaVDR.Commands }});
 });
