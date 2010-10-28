75bd81606ee7fb5eb7883fb5141325669fe4e596
traxanos <traxanos@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-10-28 21:17:35 +0000

    style system and add commands
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@2218 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/yavdr-utils/yavdrweb-ng/static/ng/static/javascripts/component/system.js
+++ utils/yavdr-utils/yavdrweb-ng/static/ng/static/javascripts/component/system.js
@@ -5,8 +5,59 @@ YaVDR.Component.System = Ext.extend(YaVDR.Component, {
   initComponent: function() {
     this.items = [
       new YaVDR.Component.Item({
-        title: 'Diagnose',
+        title: 'Befehle',
         style: 'padding-bottom: 5px',
+        defaults: {
+          anchor: '100%',
+          margins: '5 0 0 0',
+          layout: 'hbox',
+          defaults: {
+            xtype: 'button',
+            margins:'0 5 5 0',
+            height: 40,
+            scale: 'medium',
+            flex: 0.5
+          }
+        },
+        items: [
+
+          {
+
+            items: [
+              {
+                icon: '/icons/fugue/monitor--arrow.png',
+                text: 'VDR temporär auf 2. Bildschirm',
+                scope: this,
+                handler: this.changeVdrToSecondDisplay
+              },
+              {
+                icon: '/icons/fugue/arrow-circle-225.png',
+                text: 'Rechner neustarten',
+                scope: this,
+                handler: this.reboot
+              }
+            ]
+          },
+          {
+            items: [
+              {
+                icon: '/icons/fugue/arrow-step.png',
+                text: 'XBMC-Beendung erzwingen',
+                scope: this,
+                handler: this.killXBMC
+              },
+              {
+                icon: '/icons/fugue/arrow-circle-double-135.png',
+                text: 'VDR-Backend neustarten',
+                scope: this,
+                handler: this.restartVDR
+              }
+            ]
+          }
+        ]
+      }),
+      new YaVDR.Component.Item({
+        title: 'Diagnose',
         defaults: {
           anchor: '100%',
           margins: '5 0 0 0',
@@ -28,18 +79,22 @@ YaVDR.Component.System = Ext.extend(YaVDR.Component, {
             items: [
               {
                 itemId: 'system-diagnose-system-infos',
+                icon: '/icons/fugue/system-monitor.png',
                 text: 'System-Informationen'
               },
               {
                 itemId: 'system-diagnose-system-logs',
+                icon: '/icons/fugue/script-text.png',
                 text: 'System-Logfiles'
               },
               {
                 itemId: 'system-diagnose-xbmc',
+                icon: '/icons/fugue/exclamation.png',
                 text: 'XBMC-Abstürze'
               },
               {
                 itemId: 'system-diagnose-lirc',
+                icon: '/icons/silk/keyboard.png',
                 text: 'LIRC-Konfiguration'
               }
             ]
@@ -48,18 +103,22 @@ YaVDR.Component.System = Ext.extend(YaVDR.Component, {
             items: [
               {
                 itemId: 'system-diagnose-vdr',
+                icon: '/icons/fugue/screwdriver.png',
                 text: 'VDR-Konfiguration'
               },
               {
                 itemId: 'system-diagnose-x11',
+                icon: '/icons/fugue/television.png',
                 text: 'X-Serever'
               },
               {
                 itemId: 'system-diagnose-alsa',
+                icon: '/icons/fugue/speaker.png',
                 text: 'Sound (ALSA)'
               },
               {
                 itemId: 'system-diagnose-dpkg',
+                icon: '/icons/silk/package.png',
                 text: 'Pakete'
               }
             ]
@@ -68,6 +127,7 @@ YaVDR.Component.System = Ext.extend(YaVDR.Component, {
             items: [
               {
                 itemId: 'system-diagnose-yavdr',
+                icon: '/icons/fugue/database.png',
                 text: 'yaVDR-Utils'
               },
               {
@@ -85,6 +145,35 @@ YaVDR.Component.System = Ext.extend(YaVDR.Component, {
       })
     ];
     YaVDR.Component.System.superclass.initComponent.call(this);
+  },
+  changeVdrToSecondDisplay: function() {
+    this.request('/admin/set_signal?signal=change-display');
+  },
+  stopVDR: function() {
+    this.request('/admin/set_signal?signal=stop-vdr');
+  },
+  restartVDR: function() {
+    this.request('/admin/set_signal?signal=restart-vdr');
+  },
+  killXBMC: function() {
+    this.request('/admin/set_signal?signal=kill-xbmc');
+  },
+  reboot: function() {
+    this.request('/admin/set_signal?signal=reboot');
+  },
+  request: function(url) {
+    Ext.getBody().mask("Führe Befehl aus...", 'x-mask-loading');
+    Ext.Ajax.request({
+      url: url,
+      success: function() {
+        YaVDR.notice('Führe Befehl aus', 'Der Befehl wurde ausgeführt');
+        Ext.getBody().unmask();
+      },
+      failue: function() {
+        YaVDR.alert('Führe Befehl aus', 'Der Befehl konte nicht ausgeführt werden');
+        Ext.getBody().unmask();
+      }
+    });
   }
 });
 
