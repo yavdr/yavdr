6e0663b5b2a4b041306dd57acce7f0323fa86d2e
traxanos <traxanos@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-10-31 17:45:11 +0000

    remove diagnose /etc/X11/Xsession.vdr
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@2256 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/yavdr-utils/yavdrweb-ng/static/javascripts/component/system/diagnose_x11.js
+++ utils/yavdr-utils/yavdrweb-ng/static/javascripts/component/system/diagnose_x11.js
@@ -4,7 +4,6 @@ YaVDR.Component.System.VDR = Ext.extend(YaVDR.Component.System.Diagnose, {
   initComponent: function() {
     YaVDR.Component.System.VDR.superclass.initComponent.call(this);
     this.addFileContent('X-Server Configuration (only if NVIDIA VDPAU present)', '/etc/X11/xorg.conf.yavdr');
-    this.addFileContent('XSession.vdr', '/etc/X11/Xsession.vdr');
     this.addFileContent('X-Server Logfile', '/var/log/Xorg.1.log');
   }
 });
