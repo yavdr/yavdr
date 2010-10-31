d79187e2c0e6d7d359ef80d1bd5469187ee9581e
traxanos <traxanos@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-10-31 17:43:53 +0000

    dashboard
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@2255 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/yavdr-utils/yavdrweb-ng/static/javascripts/component/dashboard.js
+++ utils/yavdr-utils/yavdrweb-ng/static/javascripts/component/dashboard.js
@@ -1,69 +1,160 @@
 YaVDR.Component.Dashboard = Ext.extend(YaVDR.Component, {
-  itemId: 'dashboard',
-  layout:'column',
-  initComponent: function() {
+	itemId: 'dashboard',
+	layout:'column',
+	initComponent: function() {
 
-    //this.padding = 5;
-    this.items = [
+		//this.padding = 5;
+		this.items = [
 
-      {
-        columnWidth: .5,
-        baseCls:'x-plain',
+			{
+				columnWidth: .5,
+				baseCls:'x-plain',
 
-        items: [
-          {
-            frame: true,
-            autoScroll: true,
-            height: 150,
-            anchor: '100%',
-            title: 'VDR',
-            style: 'margin: 0 5px 5px 0 ',
-            tools: [{id: 'refresh'}],
-            html: '<b>Status</b>: VDR gestartet<br><b>Speicherplatz</b>: 45G<br><b>Frontend</b>: Xine<br><b>Auflösung</b>: 1920x1080'
-          },
-          {
-            frame: true,
-            autoScroll: true,
-            anchor: '100%',
-            height: 150,
-            title: 'Aktuelle VDR-Aufnahme',
-            style: 'margin: 0 5px 0 0 ',
-            tools: [{id: 'refresh'}],
-            html: '* Filme 1<br>* Filme 1<br>* Filme 1<br>* Filme 1<br>* Filme 1<br>* Filme 1<br>'
-          }
-        ]
-      },
-      {
-        columnWidth: .5,
-        baseCls:'x-plain',
-        items: [
-          {
-            frame: true,
-            autoScroll: true,
-            height: 150,
-            anchor: '100%',
-            title: 'System',
-            tools: [{id: 'refresh'}],
-            style: 'margin: 0 0 5px 0 ',
-            html: '<b>Shutdown</b>: S3 (Disabled USB-Wakeup)<br> <b>CPU</b>: 10% - <b>RAM</b>: 1024MB (169 MG frei)<br><b>Soundausgabe</b>: HDMI + Analog'
-          },
-          {
-            frame: true,
-            autoScroll: true,
-            anchor: '100%',
-            height: 150,
-            title: 'Nächste Timer',
-            tools: [{id: 'refresh'}],
-            style: 'margin: 0 0 0 0 ',
-            html: '* 20:15 Film abc<br>* 20:15 Film abc<br>* 20:15 Film abc<br>* 20:15 Film abc<br>* 20:15 Film abc<br>* 20:15 Film abc<br>* 20:15 Film abc<br>* 20:15 Film abc<br>* 20:15 Film abc<br>* 20:15 Film abc<br>* 20:15 Film abc<br>* 20:15 Film abc<br>* 20:15 Film abc<br>'
-          }
-        ]
-      }
+				items: [
+					{
+						frame: true,
+						autoScroll: true,
+						height: 150,
+						anchor: '100%',
+						title: 'VDR',
+						style: 'margin: 0 5px 5px 0 ',
+						tools: [
+							{id: 'refresh'}
+						],
+						html: '<b>Status</b>: VDR gestartet<br><b>Speicherplatz</b>: 45G<br><b>Frontend</b>: Xine<br><b>Auflösung</b>: 1920x1080'
+					},
 
-    ];
+					new YaVDR.Component.Dashboard.Recordings()
+					/*
+					 {
+					 frame: true,
+					 autoScroll: true,
+					 anchor: '100%',
+					 height: 150,
+					 title: 'Aktuelle VDR-Aufnahme',
+					 style: 'margin: 0 5px 0 0 ',
+					 tools: [
+					 {id: 'refresh'}
+					 ],
+					 html: '* Filme 1<br>* Filme 1<br>* Filme 1<br>* Filme 1<br>* Filme 1<br>* Filme 1<br>'
+					 }*/
+				]
+			},
+			{
+				columnWidth: .5,
+				baseCls:'x-plain',
+				items: [
 
-    YaVDR.Component.Dashboard.superclass.initComponent.call(this);
-  }
+					{
+						frame: true,
+						autoScroll: true,
+						height: 150,
+						anchor: '100%',
+						title: 'System',
+						tools: [
+							{id: 'refresh'}
+						],
+						style: 'margin: 0 0 5px 0 ',
+						html: '<b>Shutdown</b>: S3 (Disabled USB-Wakeup)<br> <b>CPU</b>: 10% - <b>RAM</b>: 1024MB (169 MG frei)<br><b>Soundausgabe</b>: HDMI + Analog'
+					},
+					new YaVDR.Component.Dashboard.Timers()
+				]
+			}
+
+		];
+
+		YaVDR.Component.Dashboard.superclass.initComponent.call(this);
+	}
 });
 
 YaVDR.registerComponent(YaVDR.Component.Dashboard);
+
+YaVDR.Component.Dashboard.Item = Ext.extend(Ext.Panel, {
+	frame: true,
+	autoScroll: true,
+	anchor: '100%',
+	height: 250,
+	layout: 'fit',
+	initComponent: function() {
+		YaVDR.Component.Dashboard.Item.superclass.initComponent.call(this);
+	}
+});
+
+
+YaVDR.Component.Dashboard.Recordings = Ext.extend(YaVDR.Component.Dashboard.Item, {
+	title: 'Recordings',
+	style: 'margin: 0 5px 0 0 ',
+	initComponent: function() {
+
+		this.tools = [
+			{
+				id: 'refresh',
+				scope: this,
+				handler: this.reload
+			}
+		],
+
+		this.store = new YaVDR.RecordingsStore({ autoLoad: true, baseParams: { limit: 10 } });
+
+		this.gridPanel = new Ext.grid.GridPanel({
+			bodyCssClass: 'frame-border',
+			autoExpandColumn: 'name',
+			loadMask: true,
+			viewConfig: {
+				forceFit: true
+			},
+			disableSelection: true,
+			columns: [
+				{ header: 'Name', dataIndex: 'name', width: 400 },
+				{ header: 'Dauer', dataIndex: 'length', width: 100 }
+			],
+			store: this.store
+		});
+
+		this.items = [this.gridPanel];
+
+		YaVDR.Component.Dashboard.Recordings.superclass.initComponent.call(this);
+	},
+	reload: function() {
+		this.store.reload();
+	}
+});
+
+
+YaVDR.Component.Dashboard.Timers = Ext.extend(YaVDR.Component.Dashboard.Item, {
+	title: 'Timers',
+	initComponent: function() {
+
+		this.tools = [
+			{
+				id: 'refresh',
+				scope: this,
+				handler: this.reload
+			}
+		],
+
+		this.store = new YaVDR.TimersStore({ autoLoad: true, baseParams: { limit: 10 } });
+
+		this.gridPanel = new Ext.grid.GridPanel({
+			bodyCssClass: 'frame-border',
+			autoExpandColumn: 'name',
+			loadMask: true,
+			viewConfig: {
+				forceFit: true
+			},
+			disableSelection: true,
+			columns: [
+				{ header: 'Name', dataIndex: 'name', width: 400 },
+				{ header: 'Dauer', dataIndex: 'length', width: 100 }
+			],
+			store: this.store
+		});
+
+		this.items = [this.gridPanel];
+
+		YaVDR.Component.Dashboard.Timers.superclass.initComponent.call(this);
+	},
+	reload: function() {
+		this.store.reload();
+	}
+});
