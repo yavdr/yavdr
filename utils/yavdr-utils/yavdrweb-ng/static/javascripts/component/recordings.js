e56c09877334b683b6203c1599c6885d5c5d50c9
traxanos <traxanos@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-10-31 17:03:40 +0000

    recodings
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@2254 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- utils/yavdr-utils/yavdrweb-ng/static/javascripts/component/recordings.js
+++ utils/yavdr-utils/yavdrweb-ng/static/javascripts/component/recordings.js
@@ -1,9 +1,74 @@
 YaVDR.Component.Recordings = Ext.extend(YaVDR.Component, {
-  itemId: 'recordings',
-  title: 'Recordings',
-  initComponent: function() {
-    YaVDR.Component.Recordings.superclass.initComponent.call(this);
-  }
+	itemId: 'recordings',
+	layout: 'border',
+	initComponent: function() {
+
+		this.store = new YaVDR.RecordingsStore({ autoLoad: true });
+
+		this.detailPanel = new Ext.Panel({
+			bodyCssClass: 'frame-border',
+			padding: 5,
+			style: 'background-color: #FFF',
+			region: 'south',
+			split: true,
+			height: 50,
+			itemId: 'detail'
+		});
+
+		this.gridPanel = new Ext.grid.GridPanel({
+			bodyCssClass: 'frame-border',
+			tbar: [
+				{
+					icon: '/icons/silk/arrow_refresh.png',
+					text: 'Reload',
+					scope: this,
+					handler: this.reloadStore
+				}
+			],
+			viewConfig: {
+				forceFit: true
+			},
+			columns: [
+				{ header: 'Name', dataIndex: 'name' },
+				{ header: 'Dauer', dataIndex: 'length' }
+			],
+			store: this.store,
+			itemId: 'list',
+			region: 'center',
+			margins: '0 0 0 0'
+		});
+
+		this.items = [
+			new YaVDR.Component.Header({
+				html: 'Recordings',
+				region: 'north'
+			}),
+			{
+				frame: true,
+				region: 'center',
+				layout: 'border',
+				header: true,
+				//title: 'Recordings',
+				items: [
+					this.gridPanel,
+					this.detailPanel
+				]
+			}
+		];
+
+		YaVDR.Component.Recordings.superclass.initComponent.call(this);
+		this.gridPanel.getSelectionModel().on('rowselect', this.updateDetailOnSelect, this);
+		this.gridPanel.getSelectionModel().on('rowdeselect', this.updateDetailOnDeselect, this);
+	},
+	updateDetailOnDeselect: function() {
+		this.detailPanel.update("");
+	},
+	updateDetailOnSelect: function(sm, rowIndex, record) {
+		this.detailPanel.update(record.data.information);
+	},
+	reloadStore: function() {
+		this.store.reload();
+	}
 });
 
 YaVDR.registerComponent(YaVDR.Component.Recordings);
---
+++ utils/yavdr-utils/yavdrweb-ng/static/javascripts/stores/recordings.js
@@ -0,0 +1,3 @@
+YaVDR.RecordingsStore = Ext.extend(Ext.data.JsonStore, {
+	url: '/admin/get_recordings'
+});
