e8d9d450f176c72fbb8a39044cb240206aec9bb2
traxanos <traxanos@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2010-11-01 12:15:25 +0000

    cleanup code
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@2275 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- yavdr-hardware/sundtek/yavdr-hardware-sundtek/yavdrweb-ng/static/javascripts/component/settings/hw_sundtek.js
+++ yavdr-hardware/sundtek/yavdr-hardware-sundtek/yavdrweb-ng/static/javascripts/component/settings/hw_sundtek.js
@@ -1,62 +1,65 @@
 YaVDR.Component.Settings.HwSundtek = Ext.extend(YaVDR.Component, {
-  itemId: 'settings-hw-sundtek',
-  description: 'You can configure your sundtek DVB hardware here.',
-  title: 'Sundtek',
-  initComponent: function() {
-    this.items = [
-      new YaVDR.Component.Item({
-        title: 'Sundtek Settings',
-        style: 'margin-bottom: 5px',
-        items: new YaVDR.Component.Settings.HwSundtek.Hardware
-      })
-    ];
-    YaVDR.Component.Settings.HwAudio.superclass.initComponent.call(this);
-  }
+	itemId: 'settings-hw-sundtek',
+	description: 'You can configure your sundtek DVB hardware here.',
+	title: 'Sundtek',
+	initComponent: function() {
+		this.items = [
+			new YaVDR.Component.Item({
+				title: 'Sundtek Settings',
+				style: 'margin-bottom: 5px',
+				items: new YaVDR.Component.Settings.HwSundtek.Hardware
+			})
+		];
+		YaVDR.Component.Settings.HwAudio.superclass.initComponent.call(this);
+	}
 });
 YaVDR.registerComponent(YaVDR.Component.Settings.HwSundtek);
 
 
 YaVDR.Component.Settings.HwSundtek.Hardware = Ext.extend(YaVDR.Default.Form, {
-  initComponent: function() {
+	initComponent: function() {
 
-    this.sundtek = new Ext.form.RadioGroup({
-      fieldLabel: 'Sundtek DVB mode',
-      items: [
-	    {itemId: 'dvbc', boxLabel: 'DVB-C', name: 'mode', inputValue: 'DVBC'},
-	    {itemId: 'dvbt', boxLabel: 'DVB-T', name: 'mode', inputValue: 'DVBT'}
-      ]
-    });
+		this.sundtek = new Ext.form.RadioGroup({
+			fieldLabel: 'Sundtek DVB mode',
+			items: [
+				{
+					itemId: 'dvbc',
+					boxLabel: 'DVB-C',
+					name: 'mode',
+					inputValue: 'DVBC'
+				},
+				{
+					itemId: 'dvbt',
+					boxLabel: 'DVB-T',
+					name: 'mode',
+					inputValue: 'DVBT'
+				}
+			]
+		});
 
-    this.items = [
-      this.sundtek
-    ];
+		this.items = [
+			this.sundtek
+		];
 
-    YaVDR.Component.Settings.HwSundtek.Hardware.superclass.initComponent.call(this);
-  },
-  doLoad: function() {
-    Ext.Ajax.request({
-      url: '/sundtek/get_dvb',
-      method: 'GET',
-      scope: this,
-      success: function(xhr) {
-	    var currentSundtek = "";
-        try {
-          currentSundtek = xhr.responseText;
-        }
-        catch (err) {
-          Ext.MessageBox.alert( getLL("standardform.messagebox_caption.error"), 'Could not recognize current sound setting.');
-        }
-        if (currentSundtek == "DVBC" || currentSundtek == "DVBT"){
-        	this.sundtek.setValue( currentSundtek );
-        }
-      }
-    });
-  },
-  doSave: function() {
-    this.getForm().submit({
-      url: '/sundtek/set_dvb'
-    })
-  }
+		YaVDR.Component.Settings.HwSundtek.Hardware.superclass.initComponent.call(this);
+	},
+	doLoad: function() {
+		Ext.Ajax.request({
+			url: '/sundtek/get_dvb',
+			method: 'GET',
+			scope: this,
+			success: function(xhr) {
+				var currentSundtek = "";
+				currentSundtek = xhr.responseText;
+				this.sundtek.setValue(currentSundtek);
+			}
+		});
+	},
+	doSave: function() {
+		this.getForm().submit({
+			url: '/sundtek/set_dvb'
+		})
+	}
 });
 
 YaVDR.Component.Settings.addMenu('hw', 'settings-hw-sundtek', 'Sundtek', null);
