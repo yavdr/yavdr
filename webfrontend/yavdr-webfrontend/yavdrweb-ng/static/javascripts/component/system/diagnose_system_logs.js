ca22c81545666454bc8222d4c7b6557d90084f3a
traxanos <traxanos@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2011-04-11 21:05:10 +0000

    remove wfe messages log
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@2912 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- webfrontend/yavdr-webfrontend/yavdrweb-ng/static/javascripts/component/system/diagnose_system_logs.js
+++ webfrontend/yavdr-webfrontend/yavdrweb-ng/static/javascripts/component/system/diagnose_system_logs.js
@@ -3,7 +3,7 @@ YaVDR.Component.System.DiagnoseSystemLogs = Ext.extend(YaVDR.Component.System.Di
   subTitle: _('System information'),
   initComponent: function() {
     YaVDR.Component.System.DiagnoseSystemLogs.superclass.initComponent.call(this);
-    this.addFileContent('/var/log/messages', '/var/log/messages');
+    //this.addFileContent('/var/log/messages', '/var/log/messages');
     //this.addFileContent('/var/log/user.log', '/var/log/user.log');
     this.addFileContent('/var/log/syslog', '/var/log/syslog');
     this.addFileContent(_('Webserver logfile'), '/var/log/tntnet/tntnet.log');
