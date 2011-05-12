d37e04e5e28f2c0e33bb0d594ff1c9db5bd95d01
gda <gda@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2011-05-12 18:20:56 +0000

    channels added by aelo, port changed
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@3227 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- vdr-plugin-jsonapi/serverthread.cpp
+++ vdr-plugin-jsonapi/serverthread.cpp
@@ -7,6 +7,46 @@
 
 #include "serverthread.h"
 
+// Channel related
+//
+
+struct SerChannel
+{
+  std::string Name;
+  int Number;
+  int Transponder;
+  std::string Stream;
+  bool IsAtsc;
+  bool IsCable;
+  bool IsTerr;
+  bool IsSat;
+};
+
+struct SerChannels
+{
+  std::vector < struct SerChannel > channel;
+};
+
+void operator<<= (cxxtools::SerializationInfo& si, const SerChannel& c)
+{
+  si.addMember("name") <<= c.Name;
+  si.addMember("number") <<= c.Number;
+  si.addMember("transponder") <<= c.Transponder;
+  si.addMember("stream") <<= c.Stream;
+  si.addMember("is_atsc") <<= c.IsAtsc;
+  si.addMember("is_cable") <<= c.IsCable;
+  si.addMember("is_terr") <<= c.IsTerr;
+  si.addMember("is_sat") <<= c.IsSat;
+}
+
+void operator<<=(cxxtools::SerializationInfo& si, const SerChannels& c)
+{
+  si.addMember("rows") <<= c.channel;
+}
+
+// Recording related
+//
+
 struct RecordingRec
 {
   std::string Name;
@@ -34,6 +74,49 @@ void operator<<= (cxxtools::SerializationInfo& si, const RecordingsRec& p)
 {
   si.addMember("rows") <<= p.recording;
 }
+
+// ChannelsResponder
+//
+class ChannelsResponder : public cxxtools::http::Responder
+{
+  public:
+    explicit ChannelsResponder(cxxtools::http::Service& service)
+      : cxxtools::http::Responder(service)
+      { }
+    virtual void reply(std::ostream&, cxxtools::http::Request& request, cxxtools::http::Reply& reply);
+};
+
+void ChannelsResponder::reply(std::ostream& out, cxxtools::http::Request& request, cxxtools::http::Reply& reply)
+{
+  SerChannel serChannel;
+  std::vector < struct SerChannel > serChannels;
+
+  reply.addHeader("Content-Type", "application/json");
+  cxxtools::JsonSerializer serializer(out);
+
+  std::string suffix = (std::string) ".ts";  
+
+  for(cChannel *channel = Channels.First(); channel; channel = Channels.Next(channel))
+  {
+    if(!channel->GroupSep()) {
+      serChannel.Name = channel->Name();
+      serChannel.Number = channel->Number();
+      serChannel.Transponder = channel->Transponder();
+      serChannel.Stream = (std::string)channel->GetChannelID().ToString() + (std::string)suffix;
+      serChannel.IsAtsc = channel->IsAtsc();
+      serChannel.IsCable = channel->IsCable();
+      serChannel.IsSat = channel->IsSat();
+      serChannel.IsTerr = channel->IsTerr();
+      serChannels.push_back(serChannel);
+    }
+  }
+  serializer.serialize(serChannels, "rows");
+  serializer.finish();
+}
+
+// ChannelsService
+//
+typedef cxxtools::http::CachedService<ChannelsResponder> ChannelsService;
 
 // RecordingsResponder
 //
@@ -77,7 +160,7 @@ cServerThread::cServerThread ()
   active = false;
 
   listenIp = "0.0.0.0";
-  listenPort = 8001;
+  listenPort = 8002;
 
   isyslog("create server");
   server = new cxxtools::http::Server(loop, listenIp, listenPort);
@@ -100,8 +183,10 @@ cServerThread::Action(void)
 {
   active = true;
 
-  RecordingsService service;
-  server->addService("/recordings.json", service);
+  RecordingsService recordingsService;
+  ChannelsService channelsService;
+  server->addService("/recordings.json", recordingsService);
+  server->addService("/channels.json", channelsService);
   loop.run();
 
   dsyslog("JSONAPI: server thread ended (pid=%d)", getpid());
