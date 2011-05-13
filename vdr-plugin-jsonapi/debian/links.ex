3c76bbe1ccfe65b4d12341e23665bf449b679620
hotzenplotz5 <hotzenplotz5@4b10c2f0-612b-4a6c-a5b7-95c0885a526e>
2011-05-13 19:34:17 +0000

    cleanup
    git-svn-id: https://svn.origo.ethz.ch/yavdr/trunk@3235 4b10c2f0-612b-4a6c-a5b7-95c0885a526e

--- vdr-plugin-jsonapi/debian/links.ex
+++
@@ -1,1 +0,0 @@
-usr/share/vdr-plugin-jsonapi/jsonapi    var/lib/vdr/plugins/jsonapi
--- vdr-plugin-jsonapi/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(vdr-plugin-jsonapi):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="vdr-plugin-jsonapi" command="/usr/bin/vdr-plugin-jsonapi"
